ee5a85c6cbc391eb865d75fb5bacc8f4a991dac9
Michael Yeh <myeh@sw02.internal.sifive.com>
2026-01-16 23:22:08 -0800

    Use general csa1

--- src/gemm/xsfvqdotq/gemm_a1b01_i8_i8pc_i32_xsfvqdotq.c
+++ src/gemm/xsfvqdotq/gemm_a1b01_i8_i8pc_i32_xsfvqdotq.c
@@ -343,18 +343,23 @@ SKL_FUNC_PRIVATE void skl_mm_6xe32m4_aligned_i8i32_vqdotvx(
           "sf.vqdot.vx %[cvec5], %[bvec0], %[a0_5]\n"
 
           "lw %[a1_0], 0(%[a0])\n"
+          "add %[a0], %[a0], %[csa1]\n"
           "lw %[a1_1], 0(%[a1])\n"
+          "add %[a1], %[a1], %[csa1]\n"
           "lw %[a1_2], 0(%[a2])\n"
+          "add %[a2], %[a2], %[csa1]\n"
           "lw %[a1_3], 0(%[a3])\n"
+          "add %[a3], %[a3], %[csa1]\n"
           "lw %[a1_4], 0(%[a4])\n"
+          "add %[a4], %[a4], %[csa1]\n"
           "lw %[a1_5], 0(%[a5])\n"
-          "lw %[a0_0], 4(%[a0])\n"
-          "lw %[a0_1], 4(%[a1])\n"
-          "lw %[a0_2], 4(%[a2])\n"
-          "lw %[a0_3], 4(%[a3])\n"
-          "lw %[a0_4], 4(%[a4])\n"
-          "lw %[a0_5], 4(%[a5])\n"
-
+          "add %[a5], %[a5], %[csa1]\n"
+          "lw %[a0_0], 0(%[a0])\n"
+          "lw %[a0_1], 0(%[a1])\n"
+          "lw %[a0_2], 0(%[a2])\n"
+          "lw %[a0_3], 0(%[a3])\n"
+          "lw %[a0_4], 0(%[a4])\n"
+          "lw %[a0_5], 0(%[a5])\n"
 
           // pre-load first B tile (4xn) of next iteration
           "vsetvli x0, %[n4], e8, m4, ta, ma\n"
@@ -385,7 +390,7 @@ SKL_FUNC_PRIVATE void skl_mm_6xe32m4_aligned_i8i32_vqdotvx(
             [a1_4] "=&r"(a1_4), [a1_5] "=&r"(a1_5), [a0] "+&r"(a0),
             [a1] "+&r"(a1), [a2] "+&r"(a2), [a3] "+&r"(a3), [a4] "+&r"(a4),
             [a5] "+&r"(a5), [b] "+&r"(b)
-          : [csa1] "rI"(2 * csa1 * sizeof(int8_t)),
+          : [csa1] "rI"(csa1 * sizeof(int8_t)),
             [rsb1] "rI"(rsb1 * sizeof(int8_t)), [n] "r"(n), [n4] "r"(4 * n)
           : "vl", "vtype", "memory");
 
