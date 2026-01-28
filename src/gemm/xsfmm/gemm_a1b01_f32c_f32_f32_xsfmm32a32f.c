07c8385a6263ec6642e383aaa8a0368892fa6c4b
Michael Yeh <myeh@sw02.internal.sifive.com>
2026-01-28 14:39:33 -0800

    Add 1x3, 3x1

--- src/gemm/xsfmm/gemm_a1b01_f32c_f32_f32_xsfmm32a32f.c
+++ src/gemm/xsfmm/gemm_a1b01_f32c_f32_f32_xsfmm32a32f.c
@@ -142,8 +142,6 @@ SKL_FUNC_PRIVATE void skl_gemm_1tm2tn_2tm1tn_a1b01_f32c_f32_f32_xsfmm32a32f(
 
                    "sf.vtzero.t mt0\n"
                    "sf.vtzero.t mt4\n"
-                   "sf.vtzero.t mt8\n"
-                   "sf.vtzero.t mt12\n"
                    "j 2f\n"
                    "0:\n"
                    "sf.vsettnt x0, %[tn], e32, w1\n"
@@ -249,6 +247,169 @@ SKL_FUNC_PRIVATE void skl_gemm_1tm2tn_2tm1tn_a1b01_f32c_f32_f32_xsfmm32a32f(
       : "vtype", "vl", "memory");
 }
 
+SKL_XSFMM_NEW
+SKL_FUNC_PRIVATE void skl_gemm_1tm3tn_3tm1tn_a1b01_f32c_f32_f32_xsfmm32a32f(
+    size_t tm, size_t tn, size_t k, const float *a, size_t rsa, const float *b,
+    size_t rsb0, size_t csb1, float *c, size_t rsc0, size_t tsc1, bool accum,
+    bool bta) {
+  if (tm == 0 || tn == 0) {
+    return;
+  }
+
+  const size_t kShiftTile = 27;
+  const size_t kShiftPattern = 24;
+  const size_t kRowInc = 1;
+
+  const size_t tm0 = bta ? tn : tm;
+  const size_t tn0 = bta ? tm : tn;
+  const size_t tss_pattern = bta ? (size_t)(1) << kShiftPattern : 0;
+  const size_t mt0 = 0 | tss_pattern;
+  const size_t mt4 = (size_t)(4) << kShiftTile | tss_pattern;
+  const size_t mt8 = (size_t)(8) << kShiftTile | tss_pattern;
+
+  float *c0 = c;
+  float *c1 = c0 + tsc1;
+  float *c2 = c0 + 2 * tsc1;
+
+  /* Load or zero-initialize tiles. */
+  size_t mt0_load = mt0;
+  size_t mt4_load = mt4;
+  size_t mt8_load = mt8;
+
+  float *c0_load = c0;
+  float *c1_load = c1;
+  float *c2_load = c2;
+  size_t i = 0;
+  __asm__ volatile("bnez %[accum], 0f\n"
+                   "sf.vsettnt x0, %[tn0], e32, w1\n"
+                   "sf.vsettm x0, %[tm0]\n"
+
+                   "sf.vtzero.t mt0\n"
+                   "sf.vtzero.t mt4\n"
+                   "sf.vtzero.t mt8\n"
+                   "j 2f\n"
+                   "0:\n"
+                   "sf.vsettnt x0, %[tn], e32, w1\n"
+                   "1:\n"
+                   "addi %[i], %[i], 1\n"
+                   "sf.vlte32 %[mt0], (%[c0])\n"
+                   "add %[mt0], %[mt0], %[kRowInc]\n"
+                   "add %[c0], %[c0], %[sc]\n"
+                   "sf.vlte32 %[mt4], (%[c1])\n"
+                   "add %[mt4], %[mt4], %[kRowInc]\n"
+                   "add %[c1], %[c1], %[sc]\n"
+                   "sf.vlte32 %[mt8], (%[c2])\n"
+                   "add %[mt8], %[mt8], %[kRowInc]\n"
+                   "add %[c2], %[c2], %[sc]\n"
+                   "bltu %[i], %[tm], 1b\n"
+                   "2:\n"
+                   : [mt0] "+&r"(mt0_load), [mt4] "+&r"(mt4_load), [mt8] "+&r"(mt8_load),
+                     [c0] "+&r"(c0_load), [c1] "+&r"(c1_load), [c2] "+&r"(c2_load), [i] "+&r"(i)
+                   : [accum] "r"(accum), [kRowInc] "rI"(kRowInc),
+                     [sc] "r"(rsc0 * sizeof(float)), [tm] "r"(tm), [tn] "r"(tn),
+                     [tm0] "r"(tm0), [tn0] "r"(tn0)
+                   : "vtype", "vl", "memory");
+
+  /* Accumulate matrix product into tiles. */
+  const float *a0 = a;
+  const float *b0 = b;
+  const float *b1 = b0 + csb1;
+  const float *b2 = b0 + 2 * csb1;
+  __asm__ volatile(
+      "beqz %[k], 2f\n"
+
+      "sf.vsettnt x0, %[tn0], e32, w1\n"
+      "sf.vsettm x0, %[tm0]\n"
+      "sf.vsettk x0, %[k]\n"
+
+      "sf.vsettn x0, %[tm0]\n"
+      "vle32.v v0, (%[a0])\n"
+      "add %[a0], %[a0], %[sa]\n"
+      "sf.vsettn x0, %[tn0]\n"
+
+      "vle32.v v8, (%[b0])\n"
+      "add %[b0], %[b0], %[sb]\n"
+
+      "bltu %[k], %[i2], 1f\n"
+
+      "0:\n"
+      "addi %[k], %[k], -1\n"
+
+      "vle32.v v16, (%[b1])\n"
+      "add %[b1], %[b1], %[sb]\n"
+
+      "sf.mm.f.f mt0, v0, v8\n"
+
+      "vle32.v v24, (%[b2])\n"
+      "add %[b2], %[b2], %[sb]\n"
+
+      "sf.mm.f.f mt4, v0, v16\n"
+
+      "vle32.v v8, (%[b0])\n"
+      "add %[b0], %[b0], %[sb]\n"
+
+      "sf.mm.f.f mt8, v0, v24\n"
+
+      "sf.vsettn x0, %[tm0]\n"
+      "vle32.v v0, (%[a0])\n"
+      "add %[a0], %[a0], %[sa]\n"
+      "sf.vsettn x0, %[tn0]\n"
+
+      "bgeu %[k], %[i2], 0b\n"
+      "1:\n"
+
+      "vle32.v v16, (%[b1])\n"
+
+      "sf.mm.f.f mt0, v0, v8\n"
+
+      "vle32.v v24, (%[b2])\n"
+
+      "sf.mm.f.f mt4, v0, v16\n"
+      "sf.mm.f.f mt8, v0, v24\n"
+
+      "2:\n"
+      : [a0] "+&r"(a0), [b0] "+&r"(b0), [b1] "+&r"(b1), [b2] "+&r"(b2),
+        [k] "+&r"(k)
+      : [sa] "r"(rsa * sizeof(float)), [sb] "r"(rsb0 * sizeof(float)),
+        [tm0] "r"(tm0), [tn0] "r"(tn0), [i2] "r"(2)
+      : "v0", "v1", "v2", "v3", "v4", "v5", "v6", "v7", "v8", "v9", "v10",
+        "v11", "v12", "v13", "v14", "v15", "v16", "v17", "v18", "v19", "v20",
+        "v21", "v22", "v23", "v24", "v25", "v26", "v27", "v28", "v29", "v30", "v31", "vtype", "vl", "memory");
+
+  /* Store tiles to memory. */
+  size_t mt0_store = mt0;
+  size_t mt4_store = mt4;
+  size_t mt8_store = mt8;
+
+  float *c0_store = c0;
+  float *c1_store = c1;
+  float *c2_store = c2;
+  i = 0;
+  __asm__ volatile(
+      "sf.vsettnt x0, %[tn], e32, w1\n"
+
+      "0:\n"
+      "addi %[i], %[i], 1\n"
+      "sf.vste32 %[mt0], (%[c0])\n"
+      "add %[mt0], %[mt0], %[kRowInc]\n"
+      "add %[c0], %[c0], %[sc]\n"
+      "sf.vste32 %[mt4], (%[c1])\n"
+      "add %[mt4], %[mt4], %[kRowInc]\n"
+      "add %[c1], %[c1], %[sc]\n"
+      "sf.vste32 %[mt8], (%[c2])\n"
+      "add %[mt8], %[mt8], %[kRowInc]\n"
+      "add %[c2], %[c2], %[sc]\n"
+      "bltu %[i], %[tm], 0b\n"
+
+      "sf.vtdiscard"
+      : [mt0] "+&r"(mt0_store), [mt4] "+&r"(mt4_store), [mt8] "+&r"(mt8_store),
+        [c0] "+&r"(c0_store), [c1] "+&r"(c1_store), [c2] "+&r"(c2_store),
+        [i] "+&r"(i)
+      : [kRowInc] "rI"(kRowInc), [sc] "r"(rsc0 * sizeof(float)), [tm] "r"(tm),
+        [tn] "r"(tn)
+      : "vtype", "vl", "memory");
+}
+
 /* Process 2 (= 1 x 2) contiguous tm x tn tiles of c.
  * tm and tn must be <= TE.
  */
@@ -270,6 +431,30 @@ SKL_FUNC_PRIVATE void skl_gemm_2tm1tn_a1b01_f32pc_f32_f32rcp_xsfmm32a32f(
     const float *b, size_t rsb, float *c, size_t rsc0,
     size_t rsc1, bool accum) {
 skl_gemm_1tm2tn_2tm1tn_a1b01_f32c_f32_f32_xsfmm32a32f(
+    tm, tn, k, b, rsb, a, csa0, rsa1, c, rsc0, rsc1, accum, true);
+}
+
+/* Process 3 (= 1 x 3) contiguous tm x tn tiles of c.
+ * tm and tn must be <= TE.
+ */
+SKL_XSFMM_NEW
+SKL_FUNC_PRIVATE void skl_gemm_1tm3tn_a1b01_f32c_f32cp_f32rcp_xsfmm32a32f(
+    size_t tm, size_t tn, size_t k, const float *a, size_t csa,
+    const float *b, size_t rsb0, size_t csb1, float *c, size_t rsc0,
+    size_t csc1, bool accum) {
+skl_gemm_1tm3tn_3tm1tn_a1b01_f32c_f32_f32_xsfmm32a32f(
+    tm, tn, k, a, csa, b, rsb0, csb1, c, rsc0, csc1, accum, false);
+}
+
+/* Process 3 (= 3 x 1) contiguous tm x tn tiles of c.
+ * tm and tn must be <= TE.
+ */
+SKL_XSFMM_NEW
+SKL_FUNC_PRIVATE void skl_gemm_3tm1tn_a1b01_f32pc_f32_f32rcp_xsfmm32a32f(
+    size_t tm, size_t tn, size_t k, const float *a, size_t csa0, size_t rsa1,
+    const float *b, size_t rsb, float *c, size_t rsc0,
+    size_t rsc1, bool accum) {
+skl_gemm_1tm3tn_3tm1tn_a1b01_f32c_f32_f32_xsfmm32a32f(
     tm, tn, k, b, rsb, a, csa0, rsa1, c, rsc0, rsc1, accum, true);
 }
 
