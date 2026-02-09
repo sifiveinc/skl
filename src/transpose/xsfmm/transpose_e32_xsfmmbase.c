558f7481431c496334071ddcb31e026c987682f2
Michael Yeh <myeh@sw02.internal.sifive.com>
2026-02-09 15:01:22 -0800

    Save work

--- src/transpose/xsfmm/transpose_e32_xsfmmbase.c
+++ src/transpose/xsfmm/transpose_e32_xsfmmbase.c
@@ -62,3 +62,40 @@ SKL_FUNC void skl_transpose_e32_xsfmmbase(size_t m, size_t n,
   }
   __asm__ volatile("sf.vtdiscard");
 }
+
+// first prototype: one te x n row
+
+// kernel: store a0, load a1
+// assume: tm1 == tn0
+SKL_XSFMM_INOUT
+SKL_FUNC void
+skl_transpose_s1l1_e32_xsfmmbase(size_t tm0, size_t tn0, size_t tn1,
+                                 size_t tss0, size_t tss1,
+                                 uint32_t *SKL_RESTRICT a0t, size_t rsat,
+                                 const uint32_t *SKL_RESTRICT a1, size_t rsa) {
+  // assume dimensions are > 0
+  const size_t kShiftCol = 24;
+  const size_t kInc = 1;
+
+  size_t tss0 |= (size_t)1 << kShiftCol;
+  size_t tss0_end = tss0 + tn0 * kInc;
+
+  __asm__ volatile(
+      "sf.vsettnt x0, x0, e32, w1\n"
+
+      "0:\n"
+      "sf.vsettn x0, %[tm0]\n"
+      "sf.vste32 %[tss0], (%[a0t])\n"
+      "sf.vsettn x0, %[tn1]\n"
+      "sf.vlte32 %[tss1], (%[a1])\n"
+      "add %[a0t], %[a0t], %[rsat]\n"
+      "add %[a1], %[a1], %[rsa]\n"
+      "add %[tss0], %[tss0], %[kInc]\n"
+      "add %[tss1], %[tss1], %[kInc]\n"
+      "bltu %[tss0], %[tss0_end], 0b\n"
+      : [tss0] "+&r"(tss0), [tss1] "+&r"(tss1), [a0t] "+&r"(a0t), [a1] "+&r"(a1)
+      : [tm0] "r"(tm0), [tn1] "r"(tn1), [tss0_end] "r"(tss0_end),
+        [rsa] "r"(rsa * sizeof(uint32_t)), [rsat] "r"(rsat * sizeof(uint32_t)),
+        [kInc] "rI"(kInc)
+      : "vtype", "vl", "memory");
+}
