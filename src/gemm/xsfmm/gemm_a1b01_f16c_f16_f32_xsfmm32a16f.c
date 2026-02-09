4950f6c4a91fbdf28b4880c7104214c5a45a075e
Michael Yeh <myeh@sw02.internal.sifive.com>
2026-02-09 11:03:39 -0800

    Try 2tn2tn

--- src/gemm/xsfmm/gemm_a1b01_f16c_f16_f32_xsfmm32a16f.c
+++ src/gemm/xsfmm/gemm_a1b01_f16c_f16_f32_xsfmm32a16f.c
@@ -1218,6 +1218,259 @@ SKL_FUNC_PRIVATE void skl_gemm_2tm2tn_a1b01_f16pc_f16cp_f32rcp_xsfmm32a16f(
 }
 
 SKL_XSFMM_NEW
+SKL_FUNC_PRIVATE void skl_gemm_2tn2tn_a1b01_f16pc_f16cp_f32rcp_xsfmm32a16f(
+    size_t tn, size_t k, const _Float16 *a, size_t csa0, size_t rsa1,
+    const _Float16 *b, size_t rsb0, size_t csb1, float *c, size_t rsc0,
+    size_t rsc1, size_t csc1, bool accum) {
+  size_t tm = tn;
+
+  if (tm == 0 || tn == 0) {
+    return;
+  }
+
+  const size_t kShiftTile = 27;
+  const size_t kRowInc = 1;
+
+  const size_t mt0 = 0;
+  const size_t mt4 = (size_t)(4) << kShiftTile;
+  const size_t mt8 = (size_t)(8) << kShiftTile;
+  const size_t mt12 = (size_t)(12) << kShiftTile;
+
+  float *c00 = c;
+  float *c01 = c00 + csc1;
+  float *c10 = c00 + rsc1;
+  float *c11 = c10 + csc1;
+
+  /* Load or zero-initialize tiles. */
+  size_t mt0_load = mt0;
+  size_t mt4_load = mt4;
+  size_t mt8_load = mt8;
+  size_t mt12_load = mt12;
+
+  float *c00_load = c00;
+  float *c01_load = c01;
+  float *c10_load = c10;
+  float *c11_load = c11;
+  size_t i = 0;
+  __asm__ volatile(
+      "sf.vsettnt x0, %[tn], e16, w2\n"
+      "sf.vsettm x0, %[tm]\n"
+
+      "bnez %[accum], 0f\n"
+      "sf.vtzero.t mt0\n"
+      "sf.vtzero.t mt4\n"
+      "sf.vtzero.t mt8\n"
+      "sf.vtzero.t mt12\n"
+      "j 1f\n"
+      "0:\n"
+      "addi %[i], %[i], 1\n"
+      "sf.vlte32 %[mt0], (%[c00])\n"
+      "add %[mt0], %[mt0], %[kRowInc]\n"
+      "add %[c00], %[c00], %[sc]\n"
+      "sf.vlte32 %[mt4], (%[c01])\n"
+      "add %[mt4], %[mt4], %[kRowInc]\n"
+      "add %[c01], %[c01], %[sc]\n"
+      "sf.vlte32 %[mt8], (%[c10])\n"
+      "add %[mt8], %[mt8], %[kRowInc]\n"
+      "add %[c10], %[c10], %[sc]\n"
+      "sf.vlte32 %[mt12], (%[c11])\n"
+      "add %[mt12], %[mt12], %[kRowInc]\n"
+      "add %[c11], %[c11], %[sc]\n"
+      "bltu %[i], %[tm], 0b\n"
+      "1:\n"
+      : [mt0] "+&r"(mt0_load), [mt4] "+&r"(mt4_load), [mt8] "+&r"(mt8_load),
+        [mt12] "+&r"(mt12_load), [c00] "+&r"(c00_load), [c01] "+&r"(c01_load),
+        [c10] "+&r"(c10_load), [c11] "+&r"(c11_load), [i] "+&r"(i)
+      : [accum] "r"(accum), [kRowInc] "rI"(kRowInc),
+        [sc] "r"(rsc0 * sizeof(float)), [tm] "r"(tm), [tn] "r"(tn)
+      : "vtype", "vl", "memory");
+
+  /* Accumulate matrix product into tiles. */
+  // a0_{0,1} are used to load a tm x 2 submatrix of A,
+  // b0_{0,1} are used to load a 2 x tn submatrix of B,
+  // and similarly for a1_{0,1} and b1_{0,1}.
+  const _Float16 *a0_0 = a;
+  const _Float16 *a0_1 = a0_0 + csa0;
+  const _Float16 *a1_0 = a0_0 + rsa1;
+  const _Float16 *a1_1 = a1_0 + csa0;
+  const _Float16 *b0_0 = b;
+  const _Float16 *b0_1 = b0_0 + rsb0;
+  const _Float16 *b1_0 = b0_0 + csb1;
+  const _Float16 *b1_1 = b1_0 + rsb0;
+  if (k < 2) {
+    __asm__ volatile("beqz %[k], 0f\n"
+
+                     "sf.vsettnt x0, %[tn], e16, w2\n"
+                     "sf.vsettm x0, %[tm]\n"
+                     "sf.vsettk x0, %[k]\n"
+
+                     // k == 1
+                     "vle16.v v0, (%[a0_0])\n"
+
+                     "vle16.v v16, (%[b0_0])\n"
+
+                     "sf.mm.f.f mt0, v0, v16\n"
+
+                     "vle16.v v24, (%[b1_0])\n"
+
+                     "sf.mm.f.f mt4, v0, v24\n"
+
+                     "vle16.v v8, (%[a1_0])\n"
+
+                     "sf.mm.f.f mt8, v8, v16\n"
+                     "sf.mm.f.f mt12, v8, v24\n"
+
+                     "0:\n"
+                     :
+                     : [a0_0] "r"(a0_0), [a1_0] "r"(a1_0), [b0_0] "r"(b0_0),
+                       [b1_0] "r"(b1_0), [tm] "r"(tm), [tn] "r"(tn), [k] "r"(k)
+                     : "v0", "v1", "v2", "v3", "v8", "v9", "v10", "v11", "v16",
+                       "v17", "v18", "v19", "v24", "v25", "v26", "v27", "vtype",
+                       "vl", "memory");
+  } else {
+    __asm__ volatile(
+        "sf.vsettnt x0, %[tn], e16, w2\n"
+        "sf.vsettm x0, %[tm]\n"
+        "sf.vsettk x0, %[k]\n"
+
+        "vle16.v v16, (%[b0_0])\n"
+        "add %[b0_0], %[b0_0], %[sb]\n"
+        "vle16.v v20, (%[b0_1])\n"
+        "add %[b0_1], %[b0_1], %[sb]\n"
+
+        "vle16.v v0, (%[a0_0])\n"
+        "add %[a0_0], %[a0_0], %[sa]\n"
+        "vle16.v v4, (%[a0_1])\n"
+        "add %[a0_1], %[a0_1], %[sa]\n"
+
+        "bltu %[k], %[i4], 1f\n"
+
+        "0:\n"
+        "addi %[k], %[k], -2\n"
+        "vle16.v v24, (%[b1_0])\n"
+        "add %[b1_0], %[b1_0], %[sb]\n"
+        "vle16.v v28, (%[b1_1])\n"
+        "add %[b1_1], %[b1_1], %[sb]\n"
+
+        "sf.mm.f.f mt0, v0, v16\n"
+
+        "vle16.v v8, (%[a1_0])\n"
+        "add %[a1_0], %[a1_0], %[sa]\n"
+        "vle16.v v12, (%[a1_1])\n"
+        "add %[a1_1], %[a1_1], %[sa]\n"
+
+        "sf.mm.f.f mt4, v0, v24\n"
+
+        "vle16.v v0, (%[a0_0])\n"
+        "add %[a0_0], %[a0_0], %[sa]\n"
+        "vle16.v v4, (%[a0_1])\n"
+        "add %[a0_1], %[a0_1], %[sa]\n"
+
+        "sf.mm.f.f mt8, v8, v16\n"
+
+        "vle16.v v16, (%[b0_0])\n"
+        "add %[b0_0], %[b0_0], %[sb]\n"
+        "vle16.v v20, (%[b0_1])\n"
+        "add %[b0_1], %[b0_1], %[sb]\n"
+
+        "sf.mm.f.f mt12, v8, v24\n"
+        "bgeu %[k], %[i4], 0b\n"
+
+        "1:\n"
+        "addi %[k], %[k], -2\n"
+        "vle16.v v24, (%[b1_0])\n"
+        "add %[b1_0], %[b1_0], %[sb]\n"
+        "vle16.v v28, (%[b1_1])\n"
+
+        "sf.mm.f.f mt0, v0, v16\n"
+
+        "vle16.v v8, (%[a1_0])\n"
+        "add %[a1_0], %[a1_0], %[sa]\n"
+        "vle16.v v12, (%[a1_1])\n"
+
+        "sf.mm.f.f mt4, v0, v24\n"
+
+        "beqz %[k], 2f\n"
+
+        // k % 2 == 1
+        "vle16.v v0, (%[a0_0])\n"
+
+        "sf.mm.f.f mt8, v8, v16\n"
+
+        "vle16.v v16, (%[b0_0])\n"
+
+        "sf.mm.f.f mt12, v8, v24\n"
+
+        "vle16.v v24, (%[b1_0])\n"
+
+        "sf.vsettk x0, %[k]\n"
+        "sf.mm.f.f mt0, v0, v16\n"
+
+        "vle16.v v8, (%[a1_0])\n"
+
+        "sf.mm.f.f mt4, v0, v24\n"
+        "sf.mm.f.f mt8, v8, v16\n"
+        "sf.mm.f.f mt12, v8, v24\n"
+        "j 3f\n"
+
+        "2:\n" // k % 2 == 0
+        "sf.mm.f.f mt8, v8, v16\n"
+        "sf.mm.f.f mt12, v8, v24\n"
+
+        "3:\n"
+        : [a0_0] "+&r"(a0_0), [a0_1] "+&r"(a0_1), [a1_0] "+&r"(a1_0),
+          [a1_1] "+&r"(a1_1), [b0_0] "+&r"(b0_0), [b0_1] "+&r"(b0_1),
+          [b1_0] "+&r"(b1_0), [b1_1] "+&r"(b1_1), [k] "+&r"(k)
+        : [sa] "r"(2 * csa0 * sizeof(_Float16)),
+          [sb] "r"(2 * rsb0 * sizeof(_Float16)), [tm] "r"(tm), [tn] "r"(tn),
+          [i4] "r"(4)
+        : "v0", "v1", "v2", "v3", "v4", "v5", "v6", "v7", "v8", "v9", "v10",
+          "v11", "v12", "v13", "v14", "v15", "v16", "v17", "v18", "v19", "v20",
+          "v21", "v22", "v23", "v24", "v25", "v26", "v27", "v28", "v29", "v30",
+          "v31", "vtype", "vl", "memory");
+  }
+
+  /* Store tiles to memory. */
+  size_t mt0_store = mt0;
+  size_t mt4_store = mt4;
+  size_t mt8_store = mt8;
+  size_t mt12_store = mt12;
+
+  float *c00_store = c00;
+  float *c01_store = c01;
+  float *c10_store = c10;
+  float *c11_store = c11;
+  i = 0;
+  __asm__ volatile(
+      "sf.vsettnt x0, %[tn], e32, w1\n"
+
+      "0:\n"
+      "addi %[i], %[i], 1\n"
+      "sf.vste32 %[mt0], (%[c00])\n"
+      "add %[mt0], %[mt0], %[kRowInc]\n"
+      "add %[c00], %[c00], %[sc]\n"
+      "sf.vste32 %[mt4], (%[c01])\n"
+      "add %[mt4], %[mt4], %[kRowInc]\n"
+      "add %[c01], %[c01], %[sc]\n"
+      "sf.vste32 %[mt8], (%[c10])\n"
+      "add %[mt8], %[mt8], %[kRowInc]\n"
+      "add %[c10], %[c10], %[sc]\n"
+      "sf.vste32 %[mt12], (%[c11])\n"
+      "add %[mt12], %[mt12], %[kRowInc]\n"
+      "add %[c11], %[c11], %[sc]\n"
+      "bltu %[i], %[tm], 0b\n"
+
+      "sf.vtdiscard"
+      : [mt0] "+&r"(mt0_store), [mt4] "+&r"(mt4_store), [mt8] "+&r"(mt8_store),
+        [mt12] "+&r"(mt12_store), [c00] "+&r"(c00_store),
+        [c01] "+&r"(c01_store), [c10] "+&r"(c10_store), [c11] "+&r"(c11_store),
+        [i] "+&r"(i)
+      : [kRowInc] "rI"(kRowInc), [sc] "r"(rsc0 * sizeof(float)), [tm] "r"(tm),
+        [tn] "r"(tn)
+      : "vtype", "vl", "memory");
+}
+
+SKL_XSFMM_NEW
 SKL_FUNC void skl_gemm_a1b01_f16c_f16_f32_xsfmm32a16f(
     size_t m, size_t n, size_t k, const _Float16 *a, size_t csa,
     const _Float16 *b, size_t rsb, float *c, size_t rsc, bool accum) {
