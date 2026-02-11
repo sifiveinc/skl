b840ad7add0f30cd105bbd1632127963da235d23
Michael Yeh <myeh@sw02.internal.sifive.com>
2026-02-11 13:35:09 -0800

    Rename c_{ref,test}

--- test/gemm/gemm_a1b01_i8_i8pc_i32_xsfvqdotq.c
+++ test/gemm/gemm_a1b01_i8_i8pc_i32_xsfvqdotq.c
@@ -96,8 +96,8 @@ _Alignas(ALIGN) int8_t b[BLEN];
 _Alignas(ALIGN) int8_t b_pack[BLEN_PACKED];
 _Alignas(ALIGN) int32_t c[CLEN];
 #if defined(ENABLE_TEST)
-_Alignas(ALIGN) int32_t c_ref[CLEN];
-_Alignas(ALIGN) int32_t c_test[CLEN];
+_Alignas(ALIGN) int32_t ref_c[CLEN];
+_Alignas(ALIGN) int32_t test_c[CLEN];
 #endif // ENABLE_TEST
 
 #if defined(ENABLE_TEST)
@@ -108,16 +108,16 @@ _Alignas(ALIGN) int32_t c_test[CLEN];
 int check_error(void) {
   /* Compute the reference (scalar) matrix output. */
   skl_gemm_i8rc_i8rc_i32rc_scalar(M, N, K, ALPHA, a, (size_t)RSA, (size_t)CSA,
-                                  b, (size_t)RSB, (size_t)CSB, BETA, c_ref,
+                                  b, (size_t)RSB, (size_t)CSB, BETA, ref_c,
                                   (size_t)RSC, (size_t)CSC);
 
   /* Compare the reference and test outputs. */
   for (size_t i = 0; i < M; ++i) {
     for (size_t j = 0; j < RSC; ++j) {
       size_t idx = i * RSC + j * CSC;
-      if (c_test[idx] != c_ref[idx]) {
-        printf("result [%zu, %zu] (%d) != reference (%d)\n", i, j, c_test[idx],
-               c_ref[idx]);
+      if (test_c[idx] != ref_c[idx]) {
+        printf("result [%zu, %zu] (%d) != reference (%d)\n", i, j, test_c[idx],
+               ref_c[idx]);
         return 1;
       }
     }
@@ -157,10 +157,10 @@ int main(void) {
 
 #if defined(ENABLE_TEST)
   /* Make copies of C to write the reference and test outputs to. */
-  memcpy(c_ref, c, CLEN * sizeof(int32_t));
-  memcpy(c_test, c, CLEN * sizeof(int32_t));
+  memcpy(ref_c, c, CLEN * sizeof(int32_t));
+  memcpy(test_c, c, CLEN * sizeof(int32_t));
   skl_gemm_a1b01_i8_i8pc_i32_xsfvqdotq(M, N, K, a, (size_t)RSA, b_pack,
-                                       (size_t)RSB1, c_test, (size_t)RSC,
+                                       (size_t)RSB1, test_c, (size_t)RSC,
                                        ACCUM);
   res += check_error();
 #endif // ENABLE_TEST
