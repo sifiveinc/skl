42c7e327f73077f5df1a897d65ab01f2ac29e3d0
Aaron Hutchinson <aaron.hutchinson@sifive.com>
2026-03-31 15:31:51 -0700

    Run report before cleanup

--- test/skl-test-driver.c
+++ test/skl-test-driver.c
@@ -137,20 +137,20 @@ int skl_test_driver_run_suite(skl_test_suite_t *suite) {
     }
 
   cleanup:
-    // Always run cleanup if provided
-    if (steps->cleanup != NULL) {
-      int cleanup_status = steps->cleanup(&t);
-      if (cleanup_status != SKL_TEST_PASS) {
-        skl_test_driver_log(&t, stderr, "Cleanup function failed\n");
-        t.status = SKL_TEST_FAIL;
-      }
-    }
-
     // Always run report if provided, even after failure
     if (steps->report != NULL) {
       int report_status = steps->report(&t);
       if (report_status != SKL_TEST_PASS) {
         skl_test_driver_log(&t, stderr, "Report function failed\n");
+        t.status = SKL_TEST_FAIL;
+      }
+    }
+
+    // Always run cleanup if provided
+    if (steps->cleanup != NULL) {
+      int cleanup_status = steps->cleanup(&t);
+      if (cleanup_status != SKL_TEST_PASS) {
+        skl_test_driver_log(&t, stderr, "Cleanup function failed\n");
         t.status = SKL_TEST_FAIL;
       }
     }
