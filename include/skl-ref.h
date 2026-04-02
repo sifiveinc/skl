60d8a6c2eea3e9f787af6546fc8d5df1db1ea692
Hugues de Lassus <hugues.delassus@sifive.com>
2026-04-01 19:14:59 -0700

    Clang-format

--- include/skl-ref.h
+++ include/skl-ref.h
@@ -10,18 +10,18 @@
  */
 
 #include "../ref/gemm/gemm_bf16rc_bf16rc_f32rc_scalar.h"
-#include "../ref/gemm/gemm_f8e4m3rc_f8e4m3rc_f32rc_scalar.h"
 #include "../ref/gemm/gemm_f16rc_f16rc_f16rc_scalar.h"
 #include "../ref/gemm/gemm_f16rc_f16rc_f32rc_scalar.h"
 #include "../ref/gemm/gemm_f32rc_f32rc_f32rc_scalar.h"
 #include "../ref/gemm/gemm_f64rc_f64rc_f64rc_scalar.h"
+#include "../ref/gemm/gemm_f8e4m3rc_f8e4m3rc_f32rc_scalar.h"
 #include "../ref/gemm/gemm_i8rc_i8rc_i32rc_scalar.h"
 
 #include "../ref/gemm/gemm_bf16rcprc_bf16rcprc_f32rcprc_scalar.h"
-#include "../ref/gemm/gemm_f8e4m3rcprc_f8e4m3rcprc_f32rcprc_scalar.h"
 #include "../ref/gemm/gemm_f16rcprc_f16rcprc_f32rcprc_scalar.h"
 #include "../ref/gemm/gemm_f32rcprc_f32rcprc_f32rcprc_scalar.h"
 #include "../ref/gemm/gemm_f64rcprc_f64rcprc_f64rcprc_scalar.h"
+#include "../ref/gemm/gemm_f8e4m3rcprc_f8e4m3rcprc_f32rcprc_scalar.h"
 #include "../ref/gemm/gemm_i8rcprc_i8rcprc_i32rcprc_scalar.h"
 
 /*
