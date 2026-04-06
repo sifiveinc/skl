93a8aa5d41be060c2665aea10e05495e6fb6ba28
Aaron Hutchinson <aaron.hutchinson@sifive.com>
2026-04-06 13:03:39 -0700

    Refactor get-riscv-extensions to use extension detection function

--- cmake/get-riscv-extensions.cmake
+++ cmake/get-riscv-extensions.cmake
@@ -23,68 +23,46 @@ if(${SKL_PREPROCESSOR_OUTPUT} MATCHES __riscv_v_min_vlen)
   list(APPEND SKL_QEMU_CPU_OPTIONS "vlen=${RISCV_VLEN}")
 endif()
 
+set(VALID_RISCV_EXTENSIONS "")
 set(RISCV_EXTENSIONS "")
 
-if (${SKL_PREPROCESSOR_OUTPUT} MATCHES "__riscv_zfh ")
-  # The above space is necessary to distinguish from zfhmin
-  list(APPEND SKL_QEMU_CPU_OPTIONS "zfh=true")
-  list(APPEND RISCV_EXTENSIONS "zfh")
-endif()
+macro(detect_riscv_extension EXT_NAME EXT_PREPROCESSOR_STR QEMU_CPU_OPTION)
+  # Determine whether the given RISC-V extension is present in the current
+  # environment by checking EXT_PREPROCESSOR_STR against C preprocessor output
+  # and populate QEMU options.
 
-if (${SKL_PREPROCESSOR_OUTPUT} MATCHES "__riscv_zvfh ")
-  list(APPEND SKL_QEMU_CPU_OPTIONS "zvfh=true")
-  list(APPEND RISCV_EXTENSIONS "zvfh")
-endif()
+  # *** This is a macro because it modifies lists in the parent scope. ***
 
-if (${SKL_PREPROCESSOR_OUTPUT} MATCHES "__riscv_zvfbfwma")
-  list(APPEND SKL_QEMU_CPU_OPTIONS "zvfbfwma=true")
-  list(APPEND RISCV_EXTENSIONS "zvfbfwma")
-endif()
+  list(APPEND VALID_RISCV_EXTENSIONS "${EXT_NAME}")
+  if(${SKL_PREPROCESSOR_OUTPUT} MATCHES "__riscv_${EXT_PREPROCESSOR_STR}")
+    list(APPEND RISCV_EXTENSIONS "${EXT_NAME}")
+    list(APPEND SKL_QEMU_CPU_OPTIONS "${QEMU_CPU_OPTION}")
+    message(STATUS "Extension ${EXT_NAME} is enabled.")
+  endif()
+endmacro()
 
-if(${SKL_PREPROCESSOR_OUTPUT} MATCHES __riscv_zvfbfmin)
-  list(APPEND SKL_QEMU_CPU_OPTIONS "zvfbfmin=true")
-  list(APPEND RISCV_EXTENSIONS "zvfbfmin")
-endif()
+function(assert_extension_valid EXT)
+  if(NOT ${EXT} IN_LIST VALID_RISCV_EXTENSIONS)
+    message(FATAL_ERROR "Provided RISC-V extension name is not valid: " ${EXT})
+  endif()
+endfunction()
 
-if (${SKL_PREPROCESSOR_OUTPUT} MATCHES __riscv_zve32f)
-  list(APPEND SKL_QEMU_CPU_OPTIONS "zve32f=true")
-  list(APPEND RISCV_EXTENSIONS "zve32f")
-endif()
 
-if (${SKL_PREPROCESSOR_OUTPUT} MATCHES __riscv_zve64d)
-  list(APPEND SKL_QEMU_CPU_OPTIONS "zve64d=true")
-  list(APPEND RISCV_EXTENSIONS "zve64d")
-endif()
+detect_riscv_extension("zfh" "zfh " "zfh=true") # The space is necessary to distinguish from zfhmin
+detect_riscv_extension("zvfh" "zvfh" "zvfh=true")
+detect_riscv_extension("zvfbfwma" "zvfbfwma" "zvfbfwma=true")
+detect_riscv_extension("zvfbfmin" "zvfbfmin" "zvfbfmin=true")
+detect_riscv_extension("zve32f" "zve32f" "zve32f=true")
+detect_riscv_extension("zve64d" "zve64d" "zve64d=true")
+detect_riscv_extension("zve32x" "zve32x" "zve32x=true")
+detect_riscv_extension("zvfofp4min" "zvfofp4min" "x-zvfofp4min=true")
+detect_riscv_extension("zvfofp8min" "zvfofp8min" "x-zvfofp8min=true")
 
-if (${SKL_PREPROCESSOR_OUTPUT} MATCHES __riscv_zve32x)
-  list(APPEND SKL_QEMU_CPU_OPTIONS "zve32x=true")
-  list(APPEND RISCV_EXTENSIONS "zve32x")
-endif()
-
-if (${SKL_PREPROCESSOR_OUTPUT} MATCHES __riscv_xsfmmbase)
-  list(APPEND SKL_QEMU_CPU_OPTIONS "x-xsfmmbase=true")
-  list(APPEND RISCV_EXTENSIONS "xsfmm")
-endif()
-
-if (${SKL_PREPROCESSOR_OUTPUT} MATCHES __riscv_xsfmm32a8f)
-  list(APPEND SKL_QEMU_CPU_OPTIONS "x-xsfmm32a8f=true")
-  list(APPEND RISCV_EXTENSIONS "xsfmm32a8f")
-endif()
-
-if (${SKL_PREPROCESSOR_OUTPUT} MATCHES __riscv_xsfmm32a8i)
-  list(APPEND SKL_QEMU_CPU_OPTIONS "x-xsfmm32a8i=true")
-  list(APPEND RISCV_EXTENSIONS "xsfmm32a8i")
-endif()
-
-if (${SKL_PREPROCESSOR_OUTPUT} MATCHES __riscv_xsfmm32a16f)
-  list(APPEND SKL_QEMU_CPU_OPTIONS "x-xsfmm32a16f=true")
-  list(APPEND RISCV_EXTENSIONS "xsfmm32a16f")
-endif()
-
-if (${SKL_PREPROCESSOR_OUTPUT} MATCHES __riscv_xsfmm32a32f)
-  list(APPEND SKL_QEMU_CPU_OPTIONS "x-xsfmm32a32f=true")
-  list(APPEND RISCV_EXTENSIONS "xsfmm32a32f")
-endif()
+detect_riscv_extension("xsfmmbase" "xsfmmbase" "x-xsfmmbase=true")
+detect_riscv_extension("xsfmm32a8f" "xsfmm32a8f" "x-xsfmm32a8f=true")
+detect_riscv_extension("xsfmm32a8i" "xsfmm32a8i" "x-xsfmm32a8i=true")
+detect_riscv_extension("xsfmm32a16f" "xsfmm32a16f" "x-xsfmm32a16f=true")
+detect_riscv_extension("xsfmm32a32f" "xsfmm32a32f" "x-xsfmm32a32f=true")
 
 if (${SKL_PREPROCESSOR_OUTPUT} MATCHES "__riscv_xsfmm[0-9]+t")
   string(REGEX REPLACE
@@ -96,45 +74,12 @@ if (${SKL_PREPROCESSOR_OUTPUT} MATCHES "__riscv_xsfmm[0-9]+t")
   list(APPEND SKL_QEMU_CPU_OPTIONS "x-xsfmm${XSFMM_TE}t=true")
 endif()
 
-if(${SKL_PREPROCESSOR_OUTPUT} MATCHES __riscv_xsfvfbfa)
-  list(APPEND SKL_QEMU_CPU_OPTIONS "x-xsfvfbfa=true")
-  list(APPEND RISCV_EXTENSIONS "xsfvfbfa")
-endif()
-
-if (${SKL_PREPROCESSOR_OUTPUT} MATCHES __riscv_xsfvfexpa)
-  list(APPEND SKL_QEMU_CPU_OPTIONS "x-xsfvfexpa=true")
-  list(APPEND RISCV_EXTENSIONS "xsfvfexpa")
-endif()
-
-if (${SKL_PREPROCESSOR_OUTPUT} MATCHES __riscv_xsfvfexp16e)
-  list(APPEND SKL_QEMU_CPU_OPTIONS "x-xsfvfexp16e=true")
-  list(APPEND RISCV_EXTENSIONS "xsfvfexp16e")
-endif()
-
-if (${SKL_PREPROCESSOR_OUTPUT} MATCHES __riscv_xsfvfbfexp16e)
-  list(APPEND SKL_QEMU_CPU_OPTIONS "x-xsfvfbfexp16e=true")
-  list(APPEND RISCV_EXTENSIONS "xsfvfbfexp16e")
-endif()
-
-if (${SKL_PREPROCESSOR_OUTPUT} MATCHES __riscv_xsfvfexp32e)
-  list(APPEND SKL_QEMU_CPU_OPTIONS "x-xsfvfexp32e=true")
-  list(APPEND RISCV_EXTENSIONS "xsfvfexp32e")
-endif()
-
-if (${SKL_PREPROCESSOR_OUTPUT} MATCHES __riscv_xsfvqdotq)
-  list(APPEND SKL_QEMU_CPU_OPTIONS "x-xsfvqdotq=true")
-  list(APPEND RISCV_EXTENSIONS "xsfvqdotq")
-endif()
-
-if (${SKL_PREPROCESSOR_OUTPUT} MATCHES __riscv_zvfofp8min)
-  list(APPEND SKL_QEMU_CPU_OPTIONS "x-zvfofp8min=true")
-  list(APPEND RISCV_EXTENSIONS "zvfofp8min")
-endif()
-
-if (${SKL_PREPROCESSOR_OUTPUT} MATCHES __riscv_zvfofp4min)
-  list(APPEND SKL_QEMU_CPU_OPTIONS "x-zvfofp4min=true")
-  list(APPEND RISCV_EXTENSIONS "zvfofp4min")
-endif()
+detect_riscv_extension("xsfvfbfa" "xsfvfbfa" "x-xsfvfbfa=true")
+detect_riscv_extension("xsfvfexpa" "xsfvfexpa" "x-xsfvfexpa=true")
+detect_riscv_extension("xsfvfexp16e" "xsfvfexp16e" "x-xsfvfexp16e=true")
+detect_riscv_extension("xsfvfbfexp16e" "xsfvfbfexp16e" "x-xsfvfbfexp16e=true")
+detect_riscv_extension("xsfvfexp32e" "xsfvfexp32e" "x-xsfvfexp32e=true")
+detect_riscv_extension("xsfvqdotq" "xsfvqdotq" "x-xsfvqdotq=true")
 
 string(JOIN "," SKL_QEMU_CPU_OPTIONS ${SKL_QEMU_CPU_OPTIONS})
 set(SKL_QEMU_OPTIONS -cpu ${SKL_QEMU_CPU_OPTIONS})
