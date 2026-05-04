ffe823a2cc5adeb6784cfdf611d088ea000bc19d
Eric Bavier <eric.bavier@sifive.com>
2026-05-04 08:47:26 -0700

    Ensure used vector registers are communicated to compiler.

--- src/logistic/logistic_f32_xsfvfexp32e.c
+++ src/logistic/logistic_f32_xsfvfexp32e.c
@@ -16,38 +16,40 @@ SKL_FUNC void skl_logistic_5u_f32_xsfvfexp32e(float *out, const float *in,
   size_t vl;
   const float b = -0x1.9fe370p+6f; /* Input lower bound */
   const float S = +0x1.35512ep-1f; /* Input split ratio */
+  vfloat32m8_t w, x, y, z;
 
   // clang-format off
   for (; n; in += vl, out += vl, n -= vl)
     __asm__ volatile(
       "     vsetvli  %[vl], %[n], e32, m8, ta, ma \n"
       /* 0. Load and clamp */
-      "\t   vle32.v     v8,  (%[in])     \n"
-      "\t   vfsgnj.vf   v8,  v8, %[b]    \n"
-      "\t   vfmax.vf    v8,  v8, %[b]    \n"
+      "\t   vle32.v    %[x], (%[in])          \n"
+      "\t   vfsgnj.vf  %[x], %[x], %[b]       \n"
+      "\t   vfmax.vf   %[x], %[x], %[b]       \n"
       /* 1. Split x = a - e */
-      "\t   vfmul.vf   v16,  v8, %[S]    \n"
-      "\t   vfsub.vv   v24, v16, v8      \n"
+      "\t   vfmul.vf   %[y], %[x], %[S]       \n"
+      "\t   vfsub.vv   %[z], %[y], %[x]       \n"
       /* 2. Compute o = exp(a) and p = exp(e) */
-      "\t   sf.vfexp.v v16, v16          \n"
-      "\t   sf.vfexp.v v24, v24          \n"
-      "\t   vmslt.vi    v0,  v8, 0       \n"
-      "\t   vfadd.vv    v8, v16, v24     \n"
+      "\t   sf.vfexp.v %[y], %[y]             \n"
+      "\t   sf.vfexp.v %[z], %[z]             \n"
+      "\t   vmslt.vi   %[w], %[x], 0          \n"
+      "\t   vfadd.vv   %[x], %[y], %[z]       \n"
       /* 3. Assemble numerator: n = x>0 ? p : o */
-      "\t   vmerge.vvm v16, v16, v24, v0 \n"
-      "\t   vfmv.v.f    v0, %[one]       \n"
+      "\t   vmerge.vvm %[y], %[y], %[z], %[w] \n"
+      "\t   vfmv.v.f   %[w], %[one]           \n"
       /* 4. Approximate r = 1 / (d = o + p) */
-      "\t   vfrec7.v   v24, v8 \n"
-      "\t   vfnmsac.vv  v0, v8, v24      \n"
-      "\t   vfmadd.vv  v24, v0, v24      \n"
-      "\t   vfmul.vv    v0, v0, v0       \n"
-      "\t   vfmadd.vv  v24, v0, v24      \n"
-      /* 5. Finally, n * r ~ n / d */
-      "\t   vfmul.vv   v16, v16, v24     \n"
-      "\t   vse32.v    v16, (%[out])"
-      : [vl] "=&r"(vl)
-      : [n] "r"(n), [in] "r"(in), [out] "r"(out), [S] "f"(S),
-        [b] "f"(b), [one] "f"(1.0f)
+      "\t   vfrec7.v   %[z], %[x] \n"
+      "\t   vfnmsac.vv %[w], %[x], %[z]       \n"
+      "\t   vfmadd.vv  %[z], %[w], %[z]       \n"
+      "\t   vfmul.vv   %[w], %[w], %[w]       \n"
+      "\t   vfmadd.vv  %[z], %[w], %[z]       \n"
+      /* 5. Finally, n / d ~ n * r */
+      "\t   vfmul.vv   %[y], %[y], %[z]       \n"
+      "\t   vse32.v    %[y], (%[out])"
+      : [vl] "=&r"(vl),
+	[w] "=vr"(w), [x] "=vd"(x), [y] "=vd"(y), [z] "=vd"(z)
+      : [n] "r"(n), [in] "r"(in), [out] "r"(out),
+	[b] "f"(b), [S] "f"(S), [one] "f"(1.0f)
       : "vtype", "vl", "memory");
   // clang-format on
 }
