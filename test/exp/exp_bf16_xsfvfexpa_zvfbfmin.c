3269a6c5a99133247ec7c2d08229ef7bb95cc69d
Hugues de Lassus <hugues.delassus@sifive.com>
2026-05-06 16:36:56 -0700

    Add Xsfvfexpa+Zvfbfmin BF16 harness

---
+++ test/exp/exp_bf16_xsfvfexpa_zvfbfmin.c
@@ -0,0 +1,37 @@
+// Copyright 2026 SiFive, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+#if !defined(__riscv_xsfvfexpa) || !defined(__riscv_zvfbfmin)
+#error This file requires the Xsfvfexpa and Zvfbfmin extensions
+#endif
+
+/**
+ * @brief Test cases for BF16 Exponential with Xsfvfexpa and Zvfbfmin
+ * extensions.
+ *
+ * This test uses the unary_bf16 harness.
+ */
+
+#include "elementwise/unary_bf16.h"
+#include "skl-ref.h"
+#include "skl-test-driver.h"
+#include "skl.h"
+
+#define MIN (-104.f)
+#define MAX (+89.f)
+
+unary_bf16_t tests[] = {
+#if defined(SKL_ENABLE_BENCHMARKS)
+    FUNCTION_BENCHMARKS(skl_exp_1u_bf16_xsfvfexpa_zvfbfmin, -1, 1),
+#endif
+#if defined(SKL_ENABLE_TESTS)
+    FUNCTION_TESTS(skl_exp_1u_bf16_xsfvfexpa_zvfbfmin, skl_exp_bf16_ref, MIN, MAX, 1.0f),
+#endif
+};
+
+static skl_test_suite_t suite = {.name = "skl_exp_bf16_xsfvfbfa_zvfbfmin",
+                                 .num_tests = sizeof(tests) / sizeof(tests[0]),
+                                 .test_size = sizeof(unary_bf16_t),
+                                 .tests = tests};
+
+int main(void) { return skl_test_driver_run_suite(&suite); }
