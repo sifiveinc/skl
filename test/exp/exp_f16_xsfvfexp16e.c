125fedb96a876e27b078071d1ec06ddfeabca772
Hugues de Lassus <hugues.delassus@sifive.com>
2026-05-06 22:19:10 -0700

    Add Xsfvfexp16e FP16 harness

---
+++ test/exp/exp_f16_xsfvfexp16e.c
@@ -0,0 +1,40 @@
+// Copyright 2026 SiFive, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+#if !defined(__riscv_xsfvfexp16e)
+#error This file requires the Xsfvfexp16e extension
+#endif
+
+/**
+ * @brief Test cases for FP16 Exponential with Xsfvfexp16e extension.
+ *
+ * This test uses the unary_f16 harness.
+ */
+
+#include "elementwise/unary_f16.h"
+#include "skl-ref.h"
+#include "skl-test-driver.h"
+#include "skl.h"
+
+#define MIN (-18.f)
+#define MAX (+12.f)
+
+unary_f16_t tests[] = {
+#if defined(SKL_ENABLE_BENCHMARKS)
+    FUNCTION_BENCHMARKS(skl_exp_1p022u0alt8ainf_f16_xsfvfexp16e, -1, 1),
+    FUNCTION_BENCHMARKS(skl_exp_3p16u_f16_xsfvfexp16e, -1, 1),
+#endif
+#if defined(SKL_ENABLE_TESTS)
+    FUNCTION_TESTS(skl_exp_1p022u0alt8ainf_f16_xsfvfexp16e, skl_exp_f16_ref,
+                   -0x1.ffcp2f, 0x1.ffcp2f, 2.0f),
+    FUNCTION_TESTS(skl_exp_3p16u_f16_xsfvfexp16e, skl_exp_f16_ref, MIN, MAX,
+                   4.0f),
+#endif
+};
+
+static skl_test_suite_t suite = {.name = "skl_exp_f16_xsfvfexp16e",
+                                 .num_tests = sizeof(tests) / sizeof(tests[0]),
+                                 .test_size = sizeof(unary_f16_t),
+                                 .tests = tests};
+
+int main(void) { return skl_test_driver_run_suite(&suite); }
