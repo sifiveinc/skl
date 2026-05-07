0ba75e05342653b6dfeb4f6c2834445ef1408e4e
Hugues de Lassus <hugues.delassus@sifive.com>
2026-05-06 22:54:51 -0700

    Fix copyright & license headers

--- test/exp/exp_f16_xsfvfexpa_zvfh.c
+++ test/exp/exp_f16_xsfvfexpa_zvfh.c
@@ -1,5 +1,7 @@
-// Copyright 2026 SiFive, Inc.
-// SPDX-License-Identifier: Apache-2.0
+// Copyright (c) 2026 SiFive, Inc. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE file in the project root for full license information.
+// SPDX-License-Identifier: MIT
 
 #if !defined(__riscv_xsfvfexpa) || !defined(__riscv_zvfh)
 #error This file requires the Xsfvfexpa and Zvfh extensions
