107bfef7968dd1b067481792f965bfa040669acb
Eric Bavier <eric.bavier@sifive.com>
2026-05-11 12:57:22 -0700

    [Test] Ports softmax_f16 to new driver.
    * test/softmax/softmax_f16.h: New file; f16 harness declarations.
    * test/softmax/softmax_f16.c: Replace old tester with implementation
    for new harness.
    * test/softmax/skl_softmax_f16_ref.c,
    test/softmax/skl_softmax_f16_zvfh.c,
    test/softmax/skl_softmax_f16_xsfvfexpa_zvfh.c,
    test/softmax/skl_softmax_f16_xsfvfexp16e.c: New tests using
    skl-test-driver and softmax harness.
    * test/CMakeLists.txt (skl_softmax_f16_ref, skl_softmax_f16_zvfh)
    (skl_softmax_f16_xsfvfexpa_zvfh, skl_softmax_f16_xsfvfexp16e): Declare
    new tests.
    * CMakeLists.txt (softmax_f16): Remove legacy test.

--- test/softmax/softmax_f16.c
+++ test/softmax/softmax_f16.c
@@ -1,111 +1,137 @@
-// Copyright (c) 2025-2026 SiFive, Inc. All rights reserved.
+// Copyright (c) 2026 SiFive, Inc. All rights reserved.
 // Licensed under the MIT License.
 // See LICENSE file in the project root for full license information.
 // SPDX-License-Identifier: MIT
 
-#if !defined(ENABLE_TEST) && !defined(ENABLE_BENCHMARK)
-#error Must define at least one of ENABLE_TEST and ENABLE_BENCHMARK.
-#endif
-
-#if !defined(NUM_ELEMS)
-#define NUM_ELEMS 2048 // Input length
-#endif
-
-#if !defined(BETA)
-#define BETA 1.0 // Exponential scaling factor
-#endif
-
-#if !defined(TOL_ULPS)
-#define TOL_ULPS 64 // Error tolerance in ULPs
-#endif
-
-#include "skl-ref.h"
-#include "skl-test.h"
-#include "skl.h"
-#include <inttypes.h>
-#if defined(ENABLE_TEST)
+#include "softmax/softmax_f16.h"
+#include "skl-test-driver.h"
 #include <math.h>
 #include <stdlib.h>
-#endif
-#include <stdint.h>
-#include <stdio.h>
-#include <string.h>
 
-#if defined(RUN_ALL)
-#define RUN_SCALAR 1
-#define RUN_VFEXP 1
-#define RUN_VFEXPA 1
-#define RUN_ZVFH 1
-#endif
+void softmax_f16_init(skl_test_t *t) {
+  softmax_f16_t *h = (softmax_f16_t *)t->harness;
 
-enum { ALIGN = 4096 };
-__attribute__((aligned(ALIGN))) _Float16 input[NUM_ELEMS];
-__attribute__((aligned(ALIGN))) _Float16 output[NUM_ELEMS];
-__attribute__((aligned(ALIGN))) _Float16 ref_output[NUM_ELEMS];
-__attribute__((aligned(ALIGN))) double workspace[NUM_ELEMS];
+  // Allocate and initialize input buffer
+  h->a.len = h->n;
+  SKL_TEST_BUF_CREATE(t, _Float16, &h->a);
+  // Allocate output buffer, avoiding malloc(0)
+  h->ctx.s = h->n ? malloc(h->n * sizeof(_Float16)) : NULL;
 
-#if defined(ENABLE_TEST)
-/** Scalar softmax using FP64 intermediates for high accuracy */
-static void reference_softmax_f16(_Float16 *out, const _Float16 *in,
-                                  _Float16 beta, double *workspace, size_t n) {
+  if (h->steps.verify) {
+    // Allocate reference buffer
+    h->ctx.S = h->n ? malloc(h->n * sizeof(double)) : NULL;
+  }
+}
+
+typedef void (*skl_softmax_f16_t)(_Float16 *, const _Float16 *, const _Float16,
+                                  const size_t);
+
+void softmax_f16_execute(skl_test_t *t) {
+  const softmax_f16_t *h = (softmax_f16_t *)t->harness;
+  skl_softmax_f16_t fn = (skl_softmax_f16_t)(h->func);
+  fn(h->ctx.s, h->a.data, h->beta, h->n);
+}
+
+/** Reference softmax producing FP64 results for high accuracy */
+static void softmax_f16_f64(double *out, const _Float16 *in, _Float16 beta,
+                            size_t n) {
   if (n < 1)
     return;
 
-  float max = in[0];
+  _Float16 max = in[0];
   for (size_t i = 1; i < n; i++) {
-    max = fmaxf(in[i], max);
+    max = (_Float16)fmaxf(in[i], max);
   }
 
   double sum = 0;
   for (size_t i = 0; i < n; i++) {
-    workspace[i] = exp(beta * ((double)in[i] - max));
-    sum += workspace[i];
+    out[i] = exp(beta * ((double)in[i] - max));
+    sum += out[i];
   }
 
   for (size_t i = 0; i < n; i++) {
-    out[i] = (_Float16)(workspace[i] / sum);
+    out[i] /= sum;
   }
 }
-#endif // defined(ENABLE_TEST)
 
-int main(void) {
-  int ret = 0; // return value
-  printf("Measuring %d-element softmax:\n\n", NUM_ELEMS);
-  skl_test_init_f16(input, NUM_ELEMS, SKL_TEST_MIN_F16, SKL_TEST_MAX_F16);
+void softmax_f16_verify(skl_test_t *t) {
+  softmax_f16_t *h = (softmax_f16_t *)t->harness;
+  const size_t N = h->n;
+  const _Float16 min = h->a.min, max = h->a.max;
+  const _Float16 beta = h->beta;
+  size_t errs = 0;
 
-#if defined(ENABLE_TEST)
-  memset(ref_output, 0, NUM_ELEMS * sizeof(*ref_output));
-  reference_softmax_f16(ref_output, input, BETA, workspace, NUM_ELEMS);
-#define CHECK_RESULT(FUNCTION, NAME)                                           \
-  ret += skl_check_error_ulp_f16(NAME, output, ref_output, TOL_ULPS, NUM_ELEMS);
-#else
-#define CHECK_RESULT(FUNCTION, NAME)
-#endif
+  // Expected error is error of an N-element summation, i.e. `N u`,
+  // plus error introduced by stabilization and beta-scaling.  The
+  // latter subtraction and multiplication would alone introduce just
+  // `3 u` error, but this is inflated to `3β(min-max)` when passed
+  // through the exponential.
+  double u = 0x1p-11; // "unit round-off"
+  double tol = (3 * fmax(beta * fabsf(max - min), 1) + (double)N) * u;
+  tol = tol / (1 + tol); // relative to reference
 
-#define RUN(FUNCTION, NAME)                                                    \
-  memset(output, 0, NUM_ELEMS * sizeof(*output));                              \
-  SKL_BENCHMARK_RUN(NAME, NUM_ELEMS, SKL_TEST_WARMUP, FUNCTION, output, input, \
-                    BETA, NUM_ELEMS);                                          \
-  CHECK_RESULT(FUNCTION, NAME);
+  // Calculate reference result
+  softmax_f16_f64(h->ctx.S, h->a.data, beta, N);
 
-  // Run subset of functions depending on ISA compatibility
-#if defined(RUN_SCALAR)
-  RUN(skl_softmax_f16_ref, "reference");
-#endif
-#if defined(__riscv_xsfvfexp16e) && defined(RUN_VFEXP)
-  RUN(skl_softmax_f16_xsfvfexp16e, "xsfvfexp16e");
-#endif
-#if defined(__riscv_xsfvfexpa) && defined(__riscv_zvfh) && defined(RUN_VFEXPA)
-  RUN(skl_softmax_f16_xsfvfexpa_zvfh, "xsfvfexpa+zvfh");
-#endif
-#if defined(__riscv_zvfh) && defined(RUN_ZVFH)
-  RUN(skl_softmax_f16_zvfh, "zvfh");
-#endif
+  for (size_t n = 0; n < N; n++) {
+    _Float16 inp = h->a.data[n];
+    _Float16 val = h->ctx.s[n];
+    double ref = h->ctx.S[n];
+    double err = 0;
 
-#if !(defined(RUN_SCALAR) || defined(RUN_VFEXP) || defined(RUN_VFEXPA) ||      \
-      defined(RUN_ZVFH))
-#error No tests or benchmarks enabled!
-#endif
+    if (isnan(val) != isnan(ref)) {
+      SKL_TEST_LOG(t, SKL_TEST_LOG_ERROR,
+                   "[%4zd : %-16.6a]: %16.6a != ref %a (NaN mismatch)\n", n,
+                   (float)inp, (float)val, ref);
+      h->ctx.max_err = INFINITY;
+      errs++;
+    } else if (isnan(ref)) {
+      continue; // both are NaN, no error
+    } else if (ref > 0x1p-14) {
+      err = fabs(val - ref); // absolute error
+      err /= ref;            // relative error, ref always unsigned
+      if (err > tol) {
+        SKL_TEST_LOG(
+            t, SKL_TEST_LOG_ERROR,
+            "[%4zd : %-16.6a]: %16.6a !~ ref %a (%.2a rel err > %.2a)\n", n,
+            (float)inp, (float)val, ref, err, tol);
+        errs++;
+      }
+      h->ctx.max_err = (float)fmax(h->ctx.max_err, err);
+    } // else don't bother.  Many Softmax flush tiny results to
+    // zero, and relative error is problematic in the tiny realm.
+    if (errs >= 10)
+      break;
+  }
+  t->status.verify_status =
+      (h->ctx.max_err <= tol) ? SKL_TEST_PASS : SKL_TEST_FAIL;
+}
 
-  return ret > 0;
+void softmax_f16_report(skl_test_t *t) {
+  softmax_f16_t *h = (softmax_f16_t *)t->harness;
+
+#define INFO(fmt, ...) SKL_TEST_LOG(t, SKL_TEST_LOG_INFO, fmt, __VA_ARGS__)
+
+  INFO("Function: %s\n", h->name);
+  INFO("N: %zd\n", h->n);
+  INFO("Beta: %g\n", (float)h->beta);
+  if (h->steps.verify != NULL) {
+    INFO("Domain: [%g ; %g]\n", (float)h->a.min, (float)h->a.max);
+    INFO("Max rel: %.3a\n", h->ctx.max_err);
+  } else {
+    INFO("Cycles: %zd\n", t->counters.cycles);
+    INFO("Instructions: %zd\n", t->counters.instret);
+    INFO("Elements/Cycle: %f\n", (double)h->n / t->counters.cycles);
+  }
+#undef INFO
+}
+
+void softmax_f16_cleanup(skl_test_t *t) {
+  softmax_f16_t *h = (softmax_f16_t *)t->harness;
+
+  // Free buffers
+  SKL_TEST_BUF_FREE(t, &h->a);
+  free(h->ctx.s);
+  if (h->steps.verify)
+    free(h->ctx.S);
 }
---
+++ test/softmax/softmax_f16.h
@@ -0,0 +1,84 @@
+// Copyright (c) 2026 SiFive, Inc. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE file in the project root for full license information.
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+#include "skl-test-driver.h"
+#include <stddef.h>
+
+typedef struct {
+  // Test function pointers for various steps
+  skl_test_steps_t steps;
+
+  // Configurable parameters (Softmax function and arguments)
+  void *func; // polymorphic pointer to function
+  char *name; // name of the function under test
+  size_t n;   // input size
+  _Float16 beta;
+
+  // Buffer for input data
+  SKL_TEST_BUFFER(_Float16) a;
+
+  // Derived parameters & buffers (private to the test harness)
+  struct {
+    double *S;   // reference result
+    _Float16 *s; // result
+    float max_err;
+  } ctx;
+} softmax_f16_t;
+
+void softmax_f16_init(skl_test_t *);
+void softmax_f16_execute(skl_test_t *);
+void softmax_f16_verify(skl_test_t *);
+void softmax_f16_report(skl_test_t *);
+void softmax_f16_cleanup(skl_test_t *);
+
+#define BASIC_STEPS                                                            \
+  .steps = {                                                                   \
+      .init = softmax_f16_init,                                                \
+      .report = softmax_f16_report,                                            \
+      .cleanup = softmax_f16_cleanup,                                          \
+  }
+
+#define TEST                                                                   \
+  .a.mode = SKL_TEST_RANDOM, BASIC_STEPS, .steps.verify = softmax_f16_verify
+
+#define BENCHMARK .a.mode = SKL_TEST_SEQ, BASIC_STEPS, .steps.verify = NULL
+
+// clang-format off
+#define BASE_PARAMS(VAR)                                                       \
+  .func = (void *)skl_softmax_f16_##VAR,                                       \
+  .name = "skl_softmax_f16_" #VAR,                                             \
+  .steps.execute = softmax_f16_execute
+
+#define BETA_BENCHMARKS(...)                                                   \
+  {__VA_ARGS__, .beta = 1.0f},                                                 \
+  {__VA_ARGS__, .beta = 1.1f}
+
+#define VARIANT_BENCHMARKS(VAR)                                                \
+  BETA_BENCHMARKS(BENCHMARK, BASE_PARAMS(VAR), .n = 2048)
+
+
+#define BETA_TESTS(...)                                                        \
+  {__VA_ARGS__, .beta = 0x1.630p-1f16},                                        \
+  {__VA_ARGS__, .beta = 1.0f},                                                 \
+  {__VA_ARGS__, .beta = 0x1.0a3p+0f16}
+
+#define DOMAIN_TESTS(...)                                                      \
+  BETA_TESTS(__VA_ARGS__, .a.min = -1, .a.max = +1),                           \
+  BETA_TESTS(__VA_ARGS__, .a.min = +4, .a.max = +9)
+
+#define SIZE_TESTS(...)                                                        \
+  DOMAIN_TESTS(__VA_ARGS__, .n = 1),                                           \
+  DOMAIN_TESTS(__VA_ARGS__, .n = 27),                                          \
+  DOMAIN_TESTS(__VA_ARGS__, .n = 131),                                         \
+  DOMAIN_TESTS(__VA_ARGS__, .n = 380),                                         \
+  DOMAIN_TESTS(__VA_ARGS__, .n = 732),                                         \
+  DOMAIN_TESTS(__VA_ARGS__, .n = 1312)                                         \
+
+#define VARIANT_TESTS(VAR)                                                     \
+  SIZE_TESTS(TEST, BASE_PARAMS(VAR))
+
+// clang-format on
