9fc6623f416aca5016ca4a3330a992cacf9ee501
Harlen Chen <harlen.chen@sifive.com>
2026-05-12 20:06:07 -0700

    Add padding_value to API

--- test/pack/rvv/skl_pack_e8_zve32x.c
+++ test/pack/rvv/skl_pack_e8_zve32x.c
@@ -124,7 +124,7 @@ static void execute(skl_test_t *t) {
 
   skl_pack_e8rc_e8rcbrc_zve32x(h->m, h->n, h->src.data, h->rs, h->cs, h->m0,
                                h->n0, h->dst.data, h->rs0, h->cs0, h->rs1,
-                               h->cs1);
+                               h->cs1, h->padding_value);
 }
 
 int main(void) { return skl_test_driver_run_suite(&suite); }
