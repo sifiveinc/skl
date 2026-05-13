104706060ad5789be62c71d1b62cbd46226bc9f2
Aaron Hutchinson <aaron.hutchinson@sifive.com>
2026-05-13 15:24:54 -0700

    Delete reference matrix comment

--- test/gemm/gemm_bf16rcprc_bf16rcprc_f32rcprc.c
+++ test/gemm/gemm_bf16rcprc_bf16rcprc_f32rcprc.c
@@ -93,7 +93,6 @@ void gemm_bf16rcprc_bf16rcprc_f32rcprc_init(skl_test_t *t) {
 }
 
 void gemm_bf16rcprc_bf16rcprc_f32rcprc_verify(skl_test_t *t) {
-  /* Compute the reference matrix output. */
   gemm_bf16rcprc_bf16rcprc_f32rcprc_t *h =
       (gemm_bf16rcprc_bf16rcprc_f32rcprc_t *)t->harness;
 
