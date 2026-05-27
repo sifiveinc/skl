7431fa1a3091ed514d910b06025e31f0f872fcc4
Eric Bavier <eric.bavier@sifive.com>
2026-05-27 11:37:53 -0500

    [Test] Ports softmax_f32 and softmax_2d_f32 to new driver. (#83)
    This adds a new test harness for Softmax and new tests for softmax_f32
    and softmax_2d_f32 using that harness.
    
    * test/softmax/softmax_f32.h: New file; softmax harness declarations.
    * test/softmax/softmax_f32.c: Replace old tester with implementation
    for new harness.
    * test/softmax/skl_softmax_f32_ref.c,
    test/softmax/skl_softmax_f32_xsfvfexp32e.c,
    test/softmax/skl_softmax_f32_xsfvfexpa.c,
    test/softmax/skl_softmax_f32_zve32f.c,
    test/softmax/skl_softmax_2d_f32_ref.c,
    test/softmax/skl_softmax_2d_f32_xsfvfexp32e.c,
    test/softmax/skl_softmax_2d_f32_xsfvfexpa.c,
    test/softmax/skl_softmax_2d_f32_zve32f.c:  New tests using
    skl-test-driver and softmax harness.
    * test/CMakeLists.txt: Declare new tests.
    * test/softmax/softmax_2d_f32.c: Delete old 2d test.
    * CMakeLists.txt (softmax_f32, softmax_2d_f32): Remove legacy tests.

---
+++ test/softmax/skl_softmax_2d_f32_xsfvfexp32e.c
@@ -0,0 +1,33 @@
+// Copyright (c) 2026 SiFive, Inc. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE file in the project root for full license information.
+// SPDX-License-Identifier: MIT
+
+#include "skl-test-driver.h"
+#include "skl.h"
+#include "softmax/softmax_f32.h"
+
+/**
+ * @brief Test cases for 2D Softmax with Xsfvfexp32e extension
+ */
+
+// clang-format off
+softmax_f32_t tests[] = {
+#ifdef SKL_ENABLE_BENCHMARKS
+  VARIANT_2D_BENCHMARKS(xsfvfexp32e),
+#endif
+#ifdef SKL_ENABLE_TESTS
+  VARIANT_2D_TESTS(xsfvfexp32e),
+#endif
+};
+// clang-format on
+
+static skl_test_suite_t suite = {.name = "skl_softmax_2d_f32_xsfvfexp32e",
+                                 .num_tests = sizeof(tests) / sizeof(tests[0]),
+                                 .test_size = sizeof(softmax_f32_t),
+                                 .tests = tests};
+
+int main(void) {
+  // Run the suite
+  return skl_test_driver_run_suite(&suite);
+}
---
+++ test/softmax/skl_softmax_2d_f32_zve32f.c
@@ -0,0 +1,33 @@
+// Copyright (c) 2026 SiFive, Inc. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE file in the project root for full license information.
+// SPDX-License-Identifier: MIT
+
+#include "skl-test-driver.h"
+#include "skl.h"
+#include "softmax/softmax_f32.h"
+
+/**
+ * @brief Test cases for 2D Softmax with Zve32f extension
+ */
+
+// clang-format off
+softmax_f32_t tests[] = {
+#ifdef SKL_ENABLE_BENCHMARKS
+  VARIANT_2D_BENCHMARKS(zve32f),
+#endif
+#ifdef SKL_ENABLE_TESTS
+  VARIANT_2D_TESTS(zve32f),
+#endif
+};
+// clang-format on
+
+static skl_test_suite_t suite = {.name = "skl_softmax_2d_f32_zve32f",
+                                 .num_tests = sizeof(tests) / sizeof(tests[0]),
+                                 .test_size = sizeof(softmax_f32_t),
+                                 .tests = tests};
+
+int main(void) {
+  // Run the suite
+  return skl_test_driver_run_suite(&suite);
+}
