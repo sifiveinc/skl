ecb2eb3cc09354b7083c8fdcdaf90202fc2164de
Michael Yeh <myeh@sw02.internal.sifive.com>
2026-05-28 11:08:11 -0700

    Update transpose

--- src/transpose/xsfmm/transpose_e32_xsfmmbase.c
+++ src/transpose/xsfmm/transpose_e32_xsfmmbase.c
@@ -7,6 +7,7 @@
 #error This file requires the Xsfmmbase extension
 #endif
 
+#include <riscv_vector.h>
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
@@ -249,8 +250,8 @@ SKL_FUNC_PRIVATE void skl_store_tile_e32c_xsfmmbase(size_t tm, size_t tn,
 SKL_XSFMM_NEW
 SKL_FUNC_PRIVATE void skl_pack_e32_e32rcpc_xsfmmbase(
     size_t m, size_t n, const uint32_t *a, size_t rsa, size_t m0, size_t n0,
-    uint32_t *a_pack, size_t csa0, size_t rsa1, size_t csa1,
-    bool pad_right = true, bool pad_bottom = true, uint32_t padding_value = 0) {
+    uint32_t *a_pack, size_t csa0, size_t rsa1, size_t csa1, bool pad_right,
+    bool pad_bottom, uint32_t padding_value) {
   if (m == 0 || n == 0) {
     return;
   }
@@ -346,10 +347,10 @@ SKL_FUNC_PRIVATE void skl_pack_e32_e32rcpc_xsfmmbase(
   }
 }
 
-SKL_FUNC_PRIVATE void
+__attribute((unused)) SKL_FUNC_PRIVATE void
 skl_pack_tex1_e32_e32pc_xsfmmbase(size_t m, size_t n, const uint32_t *a,
                                   size_t rsa, uint32_t *a_pack, size_t csa0,
-                                  size_t rsa1, uint32_t padding_value = 0) {
+                                  size_t rsa1, uint32_t padding_value) {
   size_t te = 0;
   __asm__ volatile("sf.vsettnt %[te], x0, e32, w1"
                    : [te] "=r"(te)
@@ -368,46 +369,12 @@ SKL_FUNC void skl_transpose_e32_xsfmmbase(size_t m, size_t n,
     return;
   }
 
-  const size_t kShiftCol = 24;
-  const size_t kInc = 1;
-  size_t tm = 0;
-  size_t tn = 0;
-  for (size_t i = 0; i < m; i += tm) {
-    for (size_t j = 0; j < n; j += tn) {
-      size_t mt0_load = 0;
-      size_t mt0_store = 1 << kShiftCol;
+  size_t te = 0;
+  __asm__ volatile("sf.vsettnt %[te], x0, e32, w1"
+                   : [te] "=r"(te)
+                   :
+                   : "vtype", "vl");
 
-      const uint32_t *SKL_RESTRICT a_load = a + i * rsa + j;
-      uint32_t *SKL_RESTRICT at_store = at + j * rsat + i;
-
-      size_t k = 0;
-      __asm__ volatile(
-          "sf.vsettnt %[tn], %[avl_n], e32, w1\n"
-          "sf.vsettm %[tm], %[avl_m]\n"
-
-          "0:\n"
-          "sf.vlte32 %[mt0_load], (%[a])\n"
-          "add %[a], %[a], %[sa]\n"
-          "add %[mt0_load], %[mt0_load], %[kInc]\n"
-          "addi %[k], %[k], 1\n"
-          "bltu %[k], %[tm], 0b\n"
-
-          "li %[k], 0\n"
-          "sf.vsettn x0, %[tm]\n"
-          "1:\n"
-          "sf.vste32 %[mt0_store], (%[at])\n"
-          "add %[at], %[at], %[sat]\n"
-          "add %[mt0_store], %[mt0_store], %[kInc]\n"
-          "addi %[k], %[k], 1\n"
-          "bltu %[k], %[tn], 1b\n"
-          : [tm] "=&r"(tm), [tn] "=&r"(tn), [mt0_load] "+&r"(mt0_load),
-            [mt0_store] "+&r"(mt0_store), [a] "+&r"(a_load),
-            [at] "+&r"(at_store), [k] "+&r"(k)
-          : [avl_m] "r"(m - i), [avl_n] "r"(n - j),
-            [sa] "r"(rsa * sizeof(uint32_t)),
-            [sat] "r"(rsat * sizeof(uint32_t)), [kInc] "rI"(kInc)
-          : "vtype", "vl", "memory");
-    }
-  }
-  __asm__ volatile("sf.vtdiscard");
+  skl_pack_e32_e32rcpc_xsfmmbase(m, n, a, rsa, te, te, at, rsat, te, te * rsat,
+                                 false, false, 0);
 }
