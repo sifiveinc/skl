c65d5667f9c741b4ea838257689ab8c012a365ec
Michael Yeh <michael.yeh@sifive.com>
2026-08-06 12:52:51 -0700

    Remove _pack from reference kernels

--- ref/gemm/gemm_i8rcprc_i8rcprc_i32rcprc.c
+++ ref/gemm/gemm_i8rcprc_i8rcprc_i32rcprc.c
@@ -9,19 +9,19 @@
 
 SKL_FUNC void skl_gemm_i8rcprc_i8rcprc_i32rcprc_ref(
     size_t m0, size_t n0, size_t k0, size_t m1, size_t n1, size_t k1,
-    int32_t alpha, const int8_t *a_pack, size_t rsa0, size_t csa0, size_t rsa1,
-    size_t csa1, const int8_t *b_pack, size_t rsb0, size_t csb0, size_t rsb1,
-    size_t csb1, int32_t beta, int32_t *c_pack, size_t rsc0, size_t csc0,
+    int32_t alpha, const int8_t *a, size_t rsa0, size_t csa0, size_t rsa1,
+    size_t csa1, const int8_t *b, size_t rsb0, size_t csb0, size_t rsb1,
+    size_t csb1, int32_t beta, int32_t *c, size_t rsc0, size_t csc0,
     size_t rsc1, size_t csc1) {
   for (size_t ii1 = 0; ii1 < m1; ++ii1) {
     for (size_t jj1 = 0; jj1 < n1; ++jj1) {
-      int32_t *cp_block = c_pack + ii1 * rsc1 + jj1 * csc1;
+      int32_t *cp_block = c + ii1 * rsc1 + jj1 * csc1;
       for (size_t ii0 = 0; ii0 < m0; ++ii0) {
         for (size_t jj0 = 0; jj0 < n0; ++jj0) {
           int32_t acc = 0;
           for (size_t kk1 = 0; kk1 < k1; ++kk1) {
-            const int8_t *ap_block = a_pack + ii1 * rsa1 + kk1 * csa1;
-            const int8_t *bp_block = b_pack + kk1 * rsb1 + jj1 * csb1;
+            const int8_t *ap_block = a + ii1 * rsa1 + kk1 * csa1;
+            const int8_t *bp_block = b + kk1 * rsb1 + jj1 * csb1;
             for (size_t kk0 = 0; kk0 < k0; ++kk0) {
               int8_t a_val = ap_block[ii0 * rsa0 + kk0 * csa0];
               int8_t b_val = bp_block[kk0 * rsb0 + jj0 * csb0];
--- ref/gemm/gemm_i8rcprc_i8rcprc_i32rcprc.h
+++ ref/gemm/gemm_i8rcprc_i8rcprc_i32rcprc.h
@@ -22,18 +22,18 @@ extern "C" {
  * @param n1 - Number of columns in B and C as block matrices.
  * @param k1 - Number of columns in A and rows in B as block matrices.
  * @param alpha - Scalar multiplier for A * B product.
- * @param a_pack - Pointer to matrix A.
+ * @param a - Pointer to matrix A.
  * @param rsa0 - Row stride within each block of A in elements.
  * @param csa0 - Column stride within each block of A in elements.
  * @param rsa1 - Row stride between blocks of A in elements.
  * @param csa1 - Column stride between blocks of A in elements.
- * @param b_pack - Pointer to matrix B.
+ * @param b - Pointer to matrix B.
  * @param rsb0 - Row stride within each block of B in elements.
  * @param csb0 - Column stride within each block of B in elements.
  * @param rsb1 - Row stride between blocks of B in elements.
  * @param csb1 - Column stride between blocks of B in elements.
  * @param beta - Scalar multiplier for matrix C.
- * @param c_pack - Pointer to matrix C.
+ * @param c - Pointer to matrix C.
  * @param rsc0 - Row stride within each block of C in elements.
  * @param csc0 - Column stride within each block of C in elements.
  * @param rsc1 - Row stride between blocks of C in elements.
@@ -49,9 +49,9 @@ extern "C" {
  */
 void skl_gemm_i8rcprc_i8rcprc_i32rcprc_ref(
     size_t m0, size_t n0, size_t k0, size_t m1, size_t n1, size_t k1,
-    int32_t alpha, const int8_t *a_pack, size_t rsa0, size_t csa0, size_t rsa1,
-    size_t csa1, const int8_t *b_pack, size_t rsb0, size_t csb0, size_t rsb1,
-    size_t csb1, int32_t beta, int32_t *c_pack, size_t rsc0, size_t csc0,
+    int32_t alpha, const int8_t *a, size_t rsa0, size_t csa0, size_t rsa1,
+    size_t csa1, const int8_t *b, size_t rsb0, size_t csb0, size_t rsb1,
+    size_t csb1, int32_t beta, int32_t *c, size_t rsc0, size_t csc0,
     size_t rsc1, size_t csc1);
 
 #if defined(__cplusplus)
