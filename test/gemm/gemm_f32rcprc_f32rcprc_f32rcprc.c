20bfd9b85b3acd43bd725643a8a19b632ebd70ce
Michael Yeh <michael.yeh@sifive.com>
2026-08-13 15:59:34 -0700

    Remove comment

--- test/gemm/gemm_f32rcprc_f32rcprc_f32rcprc.c
+++ test/gemm/gemm_f32rcprc_f32rcprc_f32rcprc.c
@@ -89,7 +89,6 @@ void gemm_f32rcprc_f32rcprc_f32rcprc_init(skl_test_t *t) {
 }
 
 void gemm_f32rcprc_f32rcprc_f32rcprc_verify(skl_test_t *t) {
-  /* Compute the reference matrix output. */
   gemm_f32rcprc_f32rcprc_f32rcprc_t *h =
       (gemm_f32rcprc_f32rcprc_f32rcprc_t *)t->harness;
 
