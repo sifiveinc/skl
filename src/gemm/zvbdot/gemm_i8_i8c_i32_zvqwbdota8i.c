3d2279b0576e392682ebabce34d9797675d75c28
Michael Yeh <michael.yeh@sifive.com>
2026-08-21 18:20:04 -0700

    Add outer loop

--- src/gemm/zvbdot/gemm_i8_i8c_i32_zvqwbdota8i.c
+++ src/gemm/zvbdot/gemm_i8_i8c_i32_zvqwbdota8i.c
@@ -13,8 +13,8 @@
 
 #include "skl-common.h"
 
-SKL_FUNC void
-skl_gemm_a1b0_2xle8_i8_i8c_i32_zvqwbdota8i(size_t k, size_t n, const int8_t *a,
+SKL_FUNC_PRIVATE void
+skl_gemm_a1b0_2xle8_i8_i8c_i32_zvqwbdota8i(size_t n, size_t k, const int8_t *a,
                                            size_t rsa, const int8_t *b,
                                            size_t csb, int32_t *c, size_t rsc) {
   if (n == 0) {
@@ -24,21 +24,15 @@ skl_gemm_a1b0_2xle8_i8_i8c_i32_zvqwbdota8i(size_t k, size_t n, const int8_t *a,
   vint32m8_t cvec1 = __riscv_vmv_v_x_i32m8(0, 8);
   vint8m1_t avec = __riscv_vundefined_i8m1();
 
-  b += (n - 1) * csb;
   const int8_t *a0 = a;
   const int8_t *b0 = b;
   size_t avl = k;
   size_t vl = 0;
   uint8_t mask = 0xFFU >> (8 - n);
-  size_t jump_addr = 0;
-  size_t offset = 4 * (8 + (16 - 2 * n));
-  // seems to work if compressed instructions are not used
   __asm__ volatile(
+      "beqz %[avl], 2f\n"
       "vsetvli x0, x0, e8, m1, ta, ma\n"
       "vmv.s.x v0, %[mask]\n"
-
-      "auipc %[jump_addr], 0\n"
-      "add %[jump_addr], %[jump_addr], %[offset]\n"
 
       "0:\n"
       "mv %[a0], %[a]\n"
@@ -47,23 +41,32 @@ skl_gemm_a1b0_2xle8_i8_i8c_i32_zvqwbdota8i(size_t k, size_t n, const int8_t *a,
       "vsetvli %[vl], %[avl], e8alt, m1, ta, ma\n"
       "vle8.v %[avec], (%[a0])\n"
       "add %[a0], %[a0], %[rsa]\n"
-      "jr %[jump_addr]\n"
-      "vle8.v v15, (%[b0])\n"
-      "sub %[b0], %[b0], %[csb]\n"
-      "vle8.v v14, (%[b0])\n"
-      "sub %[b0], %[b0], %[csb]\n"
-      "vle8.v v13, (%[b0])\n"
-      "sub %[b0], %[b0], %[csb]\n"
-      "vle8.v v12, (%[b0])\n"
-      "sub %[b0], %[b0], %[csb]\n"
-      "vle8.v v11, (%[b0])\n"
-      "sub %[b0], %[b0], %[csb]\n"
-      "vle8.v v10, (%[b0])\n"
-      "sub %[b0], %[b0], %[csb]\n"
-      "vle8.v v9, (%[b0])\n"
-      "sub %[b0], %[b0], %[csb]\n"
-      "vle8.v v8, (%[b0])\n"
 
+      "vle8.v v8, (%[b0])\n"
+      "add %[b0], %[b0], %[csb]\n"
+      "beq %[n], %[i1], 1f\n"
+      "vle8.v v9, (%[b0])\n"
+      "add %[b0], %[b0], %[csb]\n"
+      "beq %[n], %[i2], 1f\n"
+      "vle8.v v10, (%[b0])\n"
+      "add %[b0], %[b0], %[csb]\n"
+      "beq %[n], %[i3], 1f\n"
+      "vle8.v v11, (%[b0])\n"
+      "add %[b0], %[b0], %[csb]\n"
+      "beq %[n], %[i4], 1f\n"
+      "vle8.v v12, (%[b0])\n"
+      "add %[b0], %[b0], %[csb]\n"
+      "beq %[n], %[i5], 1f\n"
+      "vle8.v v13, (%[b0])\n"
+      "add %[b0], %[b0], %[csb]\n"
+      "beq %[n], %[i6], 1f\n"
+      "vle8.v v14, (%[b0])\n"
+      "add %[b0], %[b0], %[csb]\n"
+      "beq %[n], %[i7], 1f\n"
+      "vle8.v v15, (%[b0])\n"
+      "add %[b0], %[b0], %[csb]\n"
+
+      "1:\n"
       "vqwbdotas.vv %[cvec0], v8, %[avec], 0, v0.t\n"
       "vle8.v %[avec], (%[a0])\n"
       "vqwbdotas.vv %[cvec1], v8, %[avec], 0, v0.t\n"
@@ -73,256 +76,117 @@ skl_gemm_a1b0_2xle8_i8_i8c_i32_zvqwbdota8i(size_t k, size_t n, const int8_t *a,
       "sub %[avl], %[avl], %[vl]\n"
 
       "bnez %[avl], 0b\n"
+      "2:\n"
       : [avec] "=&vr"(avec), [cvec0] "+&vr"(cvec0), [cvec1] "+&vr"(cvec1),
-        [jump_addr] "=&r"(jump_addr), [a0] "=&r"(a0), [b0] "=&r"(b0),
-        [a] "+&r"(a), [b] "+&r"(b), [vl] "=&r"(vl), [avl] "+&r"(avl)
-      : [mask] "r"(mask), [offset] "r"(offset), [rsa] "rI"(rsa), [csb] "rI"(csb)
+        [a0] "=&r"(a0), [b0] "=&r"(b0), [a] "+&r"(a), [b] "+&r"(b),
+        [vl] "=&r"(vl), [avl] "+&r"(avl)
+      : [mask] "r"(mask), [rsa] "rI"(rsa), [csb] "rI"(csb), [n] "r"(n),
+        [i1] "r"(1), [i2] "r"(2), [i3] "r"(3), [i4] "r"(4), [i5] "r"(5),
+        [i6] "r"(6), [i7] "r"(7)
       : "vl", "vtype", "memory", "v0", "v8", "v9", "v10", "v11", "v12", "v13",
         "v14", "v15");
 
-  __riscv_vse32_v_i32m8(c, cvec0, 8);
+  __riscv_vse32_v_i32m8(c, cvec0, n);
   c += rsc;
-  __riscv_vse32_v_i32m8(c, cvec1, 8);
+  __riscv_vse32_v_i32m8(c, cvec1, n);
 }
 
-SKL_FUNC void
-skl_gemm_a1b0_2x8_i8_i8c_i32_zvqwbdota8i(size_t k, const int8_t *a, size_t rsa,
-                                         const int8_t *b, size_t csb,
-                                         int32_t *c, size_t rsc) {
+SKL_FUNC_PRIVATE void
+skl_gemm_a1b0_1xle8_i8_i8c_i32_zvqwbdota8i(size_t n, size_t k, const int8_t *a,
+                                           size_t rsa, const int8_t *b,
+                                           size_t csb, int32_t *c) {
+  if (n == 0) {
+    return;
+  }
   vint32m8_t cvec0 = __riscv_vmv_v_x_i32m8(0, 8);
-  vint32m8_t cvec1 = __riscv_vmv_v_x_i32m8(0, 8);
-
-  size_t avl = k;
-  vint8m1_t avec;
+  vint8m1_t avec = __riscv_vundefined_i8m1();
 
   const int8_t *a0 = a;
   const int8_t *b0 = b;
+  size_t avl = k;
   size_t vl = 0;
-  __asm__ volatile("0:\n"
+  uint8_t mask = 0xFFU >> (8 - n);
+  __asm__ volatile("beqz %[avl], 2f\n"
+
+                   "vsetvli x0, x0, e8, m1, ta, ma\n"
+                   "vmv.s.x v0, %[mask]\n"
+
+                   "0:\n"
                    "mv %[a0], %[a]\n"
                    "mv %[b0], %[b]\n"
 
                    "vsetvli %[vl], %[avl], e8alt, m1, ta, ma\n"
                    "vle8.v %[avec], (%[a0])\n"
-                   "add %[a0], %[a0], %[rsa]\n"
-                   "vle8.v v0, (%[b0])\n"
+
+                   "vle8.v v8, (%[b0])\n"
                    "add %[b0], %[b0], %[csb]\n"
-                   "vle8.v v1, (%[b0])\n"
+                   "beq %[n], %[i1], 1f\n"
+                   "vle8.v v9, (%[b0])\n"
                    "add %[b0], %[b0], %[csb]\n"
-                   "vle8.v v2, (%[b0])\n"
+                   "beq %[n], %[i2], 1f\n"
+                   "vle8.v v10, (%[b0])\n"
                    "add %[b0], %[b0], %[csb]\n"
-                   "vle8.v v3, (%[b0])\n"
+                   "beq %[n], %[i3], 1f\n"
+                   "vle8.v v11, (%[b0])\n"
                    "add %[b0], %[b0], %[csb]\n"
-                   "vle8.v v4, (%[b0])\n"
+                   "beq %[n], %[i4], 1f\n"
+                   "vle8.v v12, (%[b0])\n"
                    "add %[b0], %[b0], %[csb]\n"
-                   "vle8.v v5, (%[b0])\n"
+                   "beq %[n], %[i5], 1f\n"
+                   "vle8.v v13, (%[b0])\n"
                    "add %[b0], %[b0], %[csb]\n"
-                   "vle8.v v6, (%[b0])\n"
+                   "beq %[n], %[i6], 1f\n"
+                   "vle8.v v14, (%[b0])\n"
                    "add %[b0], %[b0], %[csb]\n"
-                   "vle8.v v7, (%[b0])\n"
+                   "beq %[n], %[i7], 1f\n"
+                   "vle8.v v15, (%[b0])\n"
                    "add %[b0], %[b0], %[csb]\n"
 
-                   "vqwbdotas.vv %[cvec0], v0, %[avec], 0\n"
-                   "vle8.v %[avec], (%[a0])\n"
-                   "vqwbdotas.vv %[cvec1], v0, %[avec], 0\n"
+                   "1:\n"
+                   "vqwbdotas.vv %[cvec0], v8, %[avec], 0, v0.t\n"
 
                    "add %[a], %[a], %[vl]\n"
                    "add %[b], %[b], %[vl]\n"
                    "sub %[avl], %[avl], %[vl]\n"
 
                    "bnez %[avl], 0b\n"
-                   : [avec] "=&vr"(avec), [cvec0] "+&vr"(cvec0),
-                     [cvec1] "+&vr"(cvec1), [a0] "=&r"(a0), [b0] "=&r"(b0),
-                     [a] "+&r"(a), [b] "+&r"(b), [vl] "=&r"(vl)
-                   : [rsa] "rI"(rsa), [csb] "rI"(csb), [avl] "r"(avl)
-                   : "vl", "vtype", "memory", "v0", "v1", "v2", "v3", "v4",
-                     "v5", "v6", "v7");
+                   "2:\n"
+                   : [avec] "=&vr"(avec), [cvec0] "+&vr"(cvec0), [a0] "=&r"(a0),
+                     [b0] "=&r"(b0), [a] "+&r"(a), [b] "+&r"(b), [vl] "=&r"(vl),
+                     [avl] "+&r"(avl)
+                   : [mask] "r"(mask), [rsa] "rI"(rsa), [csb] "rI"(csb),
+                     [n] "r"(n), [i1] "r"(1), [i2] "r"(2), [i3] "r"(3),
+                     [i4] "r"(4), [i5] "r"(5), [i6] "r"(6), [i7] "r"(7)
+                   : "vl", "vtype", "memory", "v0", "v8", "v9", "v10", "v11",
+                     "v12", "v13", "v14", "v15");
 
-  __riscv_vse32_v_i32m8(c, cvec0, 8);
-  c += rsc;
-  __riscv_vse32_v_i32m8(c, cvec1, 8);
+  __riscv_vse32_v_i32m8(c, cvec0, n);
 }
 
-/*
-SKL_FUNC void skl_gemm_a1b0_vlen512_15x16_i8_i8c_i32_zvqwbdota8i(
-    size_t k, const int8_t *a, size_t rsa, const int8_t *b, size_t csb,
-    int32_t *c, size_t rsc) {
-  vint32m1_t cvec0 = __riscv_vmv_v_x_i32m1(0, 16);
-  vint32m1_t cvec1 = __riscv_vmv_v_x_i32m1(0, 16);
-  vint32m1_t cvec2 = __riscv_vmv_v_x_i32m1(0, 16);
-  vint32m1_t cvec3 = __riscv_vmv_v_x_i32m1(0, 16);
-  vint32m1_t cvec4 = __riscv_vmv_v_x_i32m1(0, 16);
-  vint32m1_t cvec5 = __riscv_vmv_v_x_i32m1(0, 16);
-  vint32m1_t cvec6 = __riscv_vmv_v_x_i32m1(0, 16);
-  vint32m1_t cvec7 = __riscv_vmv_v_x_i32m1(0, 16);
-  vint32m1_t cvec8 = __riscv_vmv_v_x_i32m1(0, 16);
-  vint32m1_t cvec9 = __riscv_vmv_v_x_i32m1(0, 16);
-  vint32m1_t cvec10 = __riscv_vmv_v_x_i32m1(0, 16);
-  vint32m1_t cvec11 = __riscv_vmv_v_x_i32m1(0, 16);
-  vint32m1_t cvec12 = __riscv_vmv_v_x_i32m1(0, 16);
-  vint32m1_t cvec13 = __riscv_vmv_v_x_i32m1(0, 16);
-  // vint32m1_t cvec14 = __riscv_vmv_v_x_i32m1(0, 16);
-
-  size_t avl = k;
-  while (avl) {
-    vint8m1_t avec;
-
-    const int8_t *a0 = a;
-    const int8_t *b0 = b;
-    size_t vl = 0;
-    __asm__ volatile(
-        "vsetvli %[vl], %[avl], e8alt, m1, ta, ma\n"
-        "vle8.v v0, (%[b0])\n"
-        "add %[b0], %[b0], %[csb]\n"
-        "vle8.v v1, (%[b0])\n"
-        "add %[b0], %[b0], %[csb]\n"
-        "vle8.v v2, (%[b0])\n"
-        "add %[b0], %[b0], %[csb]\n"
-        "vle8.v v3, (%[b0])\n"
-        "add %[b0], %[b0], %[csb]\n"
-        "vle8.v v4, (%[b0])\n"
-        "add %[b0], %[b0], %[csb]\n"
-        "vle8.v v5, (%[b0])\n"
-        "add %[b0], %[b0], %[csb]\n"
-        "vle8.v v6, (%[b0])\n"
-        "add %[b0], %[b0], %[csb]\n"
-        "vle8.v v7, (%[b0])\n"
-        "add %[b0], %[b0], %[csb]\n"
-        "vle8.v v8, (%[b0])\n"
-        "add %[b0], %[b0], %[csb]\n"
-        "vle8.v v9, (%[b0])\n"
-        "add %[b0], %[b0], %[csb]\n"
-        "vle8.v v10, (%[b0])\n"
-        "add %[b0], %[b0], %[csb]\n"
-        "vle8.v v11, (%[b0])\n"
-        "add %[b0], %[b0], %[csb]\n"
-        "vle8.v v12, (%[b0])\n"
-        "add %[b0], %[b0], %[csb]\n"
-        "vle8.v v13, (%[b0])\n"
-        "add %[b0], %[b0], %[csb]\n"
-        "vle8.v v14, (%[b0])\n"
-        "add %[b0], %[b0], %[csb]\n"
-        "vle8.v v15, (%[b0])\n"
-
-        "vle8.v %[avec], (%[a0])\n"
-        "add %[a0], %[a0], %[rsa]\n"
-        "vqwbdotas.vv %[cvec0], v0, %[avec], 0\n"
-        "vqwbdotas.vv %[cvec0], v8, %[avec], 1\n"
-
-        "vle8.v %[avec], (%[a0])\n"
-        "add %[a0], %[a0], %[rsa]\n"
-        "vqwbdotas.vv %[cvec1], v0, %[avec], 0\n"
-        "vqwbdotas.vv %[cvec1], v8, %[avec], 1\n"
-
-        "vle8.v %[avec], (%[a0])\n"
-        "add %[a0], %[a0], %[rsa]\n"
-        "vqwbdotas.vv %[cvec2], v0, %[avec], 0\n"
-        "vqwbdotas.vv %[cvec2], v8, %[avec], 1\n"
-
-        "vle8.v %[avec], (%[a0])\n"
-        "add %[a0], %[a0], %[rsa]\n"
-        "vqwbdotas.vv %[cvec3], v0, %[avec], 0\n"
-        "vqwbdotas.vv %[cvec3], v8, %[avec], 1\n"
-
-        "vle8.v %[avec], (%[a0])\n"
-        "add %[a0], %[a0], %[rsa]\n"
-        "vqwbdotas.vv %[cvec4], v0, %[avec], 0\n"
-        "vqwbdotas.vv %[cvec4], v8, %[avec], 1\n"
-
-        "vle8.v %[avec], (%[a0])\n"
-        "add %[a0], %[a0], %[rsa]\n"
-        "vqwbdotas.vv %[cvec5], v0, %[avec], 0\n"
-        "vqwbdotas.vv %[cvec5], v8, %[avec], 1\n"
-
-        "vle8.v %[avec], (%[a0])\n"
-        "add %[a0], %[a0], %[rsa]\n"
-        "vqwbdotas.vv %[cvec6], v0, %[avec], 0\n"
-        "vqwbdotas.vv %[cvec6], v8, %[avec], 1\n"
-
-        "vle8.v %[avec], (%[a0])\n"
-        "add %[a0], %[a0], %[rsa]\n"
-        "vqwbdotas.vv %[cvec7], v0, %[avec], 0\n"
-        "vqwbdotas.vv %[cvec7], v8, %[avec], 1\n"
-
-        "vle8.v %[avec], (%[a0])\n"
-        "add %[a0], %[a0], %[rsa]\n"
-        "vqwbdotas.vv %[cvec8], v0, %[avec], 0\n"
-        "vqwbdotas.vv %[cvec8], v8, %[avec], 1\n"
-
-        "vle8.v %[avec], (%[a0])\n"
-        "add %[a0], %[a0], %[rsa]\n"
-        "vqwbdotas.vv %[cvec9], v0, %[avec], 0\n"
-        "vqwbdotas.vv %[cvec9], v8, %[avec], 1\n"
-
-        "vle8.v %[avec], (%[a0])\n"
-        "add %[a0], %[a0], %[rsa]\n"
-        "vqwbdotas.vv %[cvec10], v0, %[avec], 0\n"
-        "vqwbdotas.vv %[cvec10], v8, %[avec], 1\n"
-
-        "vle8.v %[avec], (%[a0])\n"
-        "add %[a0], %[a0], %[rsa]\n"
-        "vqwbdotas.vv %[cvec11], v0, %[avec], 0\n"
-        "vqwbdotas.vv %[cvec11], v8, %[avec], 1\n"
-
-        "vle8.v %[avec], (%[a0])\n"
-        "add %[a0], %[a0], %[rsa]\n"
-        "vqwbdotas.vv %[cvec12], v0, %[avec], 0\n"
-        "vqwbdotas.vv %[cvec12], v8, %[avec], 1\n"
-
-        "vle8.v %[avec], (%[a0])\n"
-        "add %[a0], %[a0], %[rsa]\n"
-        "vqwbdotas.vv %[cvec13], v0, %[avec], 0\n"
-        "vqwbdotas.vv %[cvec13], v8, %[avec], 1\n"
-
-        // "vle8.v %[avec], (%[a0])\n"
-        // "add %[a0], %[a0], %[rsa]\n"
-        // "vqwbdotas.vv %[cvec14], v0, %[avec], 1\n"
-        // "vqwbdotas.vv %[cvec14], v8, %[avec], 0\n"
-        : [avec] "=&vr"(avec), [cvec0] "+&vr"(cvec0), [cvec1] "+&vr"(cvec1),
-          [cvec2] "+&vr"(cvec2), [cvec3] "+&vr"(cvec3), [cvec4] "+&vr"(cvec4),
-          [cvec5] "+&vr"(cvec5), [cvec6] "+&vr"(cvec6), [cvec7] "+&vr"(cvec7),
-          [cvec8] "+&vr"(cvec8), [cvec9] "+&vr"(cvec9), [cvec10] "+&vr"(cvec10),
-          [cvec11] "+&vr"(cvec11), [cvec12] "+&vr"(cvec12),
-          [cvec13] "+&vr"(cvec13), [cvec14] "+&vr"(cvec14),[a0] "+&r"(a0),
-          [b0] "+&r"(b0), [vl] "=&r"(vl)
-        : [rsa] "rI"(rsa * sizeof(int8_t)), [csb] "rI"(csb * sizeof(int8_t)),
-          [avl] "r"(avl)
-        : "vl", "vtype", "memory", "v0", "v1", "v2", "v3", "v4", "v5", "v6",
-          "v7", "v8", "v9", "v10", "v11", "v12", "v13", "v14", "v15", "v16");
-    a += vl;
-    b += vl;
-    avl -= vl;
+SKL_FUNC void skl_gemm_a1b0_i8_i8c_i32_zvqwbdota8i(size_t m, size_t n, size_t k,
+                                                   const int8_t *a, size_t rsa,
+                                                   const int8_t *b, size_t csb,
+                                                   int32_t *c, size_t rsc) {
+  if (m == 0 || n == 0) {
+    return;
   }
 
-  // write result back to C tile
-  __riscv_vse32_v_i32m1(c, cvec0, 16);
-  c += rsc;
-  __riscv_vse32_v_i32m1(c, cvec1, 16);
-  c += rsc;
-  __riscv_vse32_v_i32m1(c, cvec2, 16);
-  c += rsc;
-  __riscv_vse32_v_i32m1(c, cvec3, 16);
-  c += rsc;
-  __riscv_vse32_v_i32m1(c, cvec4, 16);
-  c += rsc;
-  __riscv_vse32_v_i32m1(c, cvec5, 16);
-  c += rsc;
-  __riscv_vse32_v_i32m1(c, cvec6, 16);
-  c += rsc;
-  __riscv_vse32_v_i32m1(c, cvec7, 16);
-  c += rsc;
-  __riscv_vse32_v_i32m1(c, cvec8, 16);
-  c += rsc;
-  __riscv_vse32_v_i32m1(c, cvec9, 16);
-  c += rsc;
-  __riscv_vse32_v_i32m1(c, cvec10, 16);
-  c += rsc;
-  __riscv_vse32_v_i32m1(c, cvec11, 16);
-  c += rsc;
-  __riscv_vse32_v_i32m1(c, cvec12, 16);
-  c += rsc;
-  __riscv_vse32_v_i32m1(c, cvec13, 16);
-  // c += rsc;
-  // __riscv_vse32_v_i32m1(c, cvec14, 16);
+  size_t i = 0;
+  for (; m - i >= 2; i += 2) {
+    size_t n_vl = 0;
+    for (size_t j = 0; j < n; j += n_vl) {
+      n_vl = n - j >= 8 ? 8 : n - j;
+      skl_gemm_a1b0_2xle8_i8_i8c_i32_zvqwbdota8i(
+          n_vl, k, a + i * rsa, rsa, b + j * csb, csb, c + i * rsc + j, rsc);
+    }
+  }
+
+  if (i < m) {
+    size_t n_vl = 0;
+    for (size_t j = 0; j < n; j += n_vl) {
+      n_vl = n - j >= 8 ? 8 : n - j;
+      skl_gemm_a1b0_1xle8_i8_i8c_i32_zvqwbdota8i(
+          n_vl, k, a + i * rsa, rsa, b + j * csb, csb, c + i * rsc + j);
+    }
+  }
 }
-*/
