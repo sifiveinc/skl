a3b0a393af773580f4d0aa9b6674ad09d63c805e
Michael Yeh <michael.yeh@sifive.com>
2026-08-21 18:20:04 -0700

    Add outer loop

--- test/gemm/zvbdot/skl_gemm_i8_i8c_i32_zvqwbdota8i.c
+++ test/gemm/zvbdot/skl_gemm_i8_i8c_i32_zvqwbdota8i.c
@@ -57,17 +57,53 @@ gemm_i8rcprc_i8rcprc_i32rcprc_t tests[] = {
 #endif // SKL_ENABLE_BENCHMARKS
 
 #ifdef SKL_ENABLE_TESTS
-    // Verification tests - comprehensive coverage for Zvqwbdota8i GEMM
+    // Verification tests
+    {TEST, .m1 = 0, .n1 = 0, .k1 = 67},
+    {TEST, .m1 = 0, .n1 = 1, .k1 = 67},
+    {TEST, .m1 = 1, .n1 = 0, .k1 = 67},
+    {TEST, .m1 = 1, .n1 = 8, .k1 = 0},
+    {TEST, .m1 = 2, .n1 = 8, .k1 = 0},
+
+    {TEST, .m1 = 1, .n1 = 1, .k1 = 67},
+    {TEST, .m1 = 1, .n1 = 2, .k1 = 67},
+    {TEST, .m1 = 1, .n1 = 3, .k1 = 67},
+    {TEST, .m1 = 1, .n1 = 4, .k1 = 67},
+    {TEST, .m1 = 1, .n1 = 5, .k1 = 67},
+    {TEST, .m1 = 1, .n1 = 6, .k1 = 67},
+    {TEST, .m1 = 1, .n1 = 7, .k1 = 67},
+    {TEST, .m1 = 1, .n1 = 8, .k1 = 67},
+    {TEST, .m1 = 1, .n1 = 15, .k1 = 67},
+
+    {TEST, .m1 = 2, .n1 = 1, .k1 = 67},
+    {TEST, .m1 = 2, .n1 = 2, .k1 = 67},
+    {TEST, .m1 = 2, .n1 = 3, .k1 = 67},
+    {TEST, .m1 = 2, .n1 = 4, .k1 = 67},
+    {TEST, .m1 = 2, .n1 = 5, .k1 = 67},
+    {TEST, .m1 = 2, .n1 = 6, .k1 = 67},
+    {TEST, .m1 = 2, .n1 = 7, .k1 = 67},
     {TEST, .m1 = 2, .n1 = 8, .k1 = 67},
+    {TEST, .m1 = 2, .n1 = 15, .k1 = 67},
+
+    {TEST, .m1 = 3, .n1 = 1, .k1 = 67},
+    {TEST, .m1 = 3, .n1 = 2, .k1 = 67},
+    {TEST, .m1 = 3, .n1 = 3, .k1 = 67},
+    {TEST, .m1 = 3, .n1 = 4, .k1 = 67},
+    {TEST, .m1 = 3, .n1 = 5, .k1 = 67},
+    {TEST, .m1 = 3, .n1 = 6, .k1 = 67},
+    {TEST, .m1 = 3, .n1 = 7, .k1 = 67},
+    {TEST, .m1 = 3, .n1 = 8, .k1 = 67},
+    {TEST, .m1 = 3, .n1 = 15, .k1 = 67},
+
+    {TEST, .m1 = 65, .n1 = 65, .k1 = 255},
 #endif // SKL_ENABLE_TESTS
 };
 // clang-format on
 
-static skl_test_suite_t suite = {
-    .name = "skl_gemm_i8_i8c_i32_zvqwbdota8i",
-    .num_tests = sizeof(tests) / sizeof(tests[0]),
-    .test_size = sizeof(gemm_i8rcprc_i8rcprc_i32rcprc_t),
-    .tests = tests};
+static skl_test_suite_t suite = {.name = "skl_gemm_i8_i8c_i32_zvqwbdota8i",
+                                 .num_tests = sizeof(tests) / sizeof(tests[0]),
+                                 .test_size =
+                                     sizeof(gemm_i8rcprc_i8rcprc_i32rcprc_t),
+                                 .tests = tests};
 
 static void init(skl_test_t *t) {
   const gemm_i8rcprc_i8rcprc_i32rcprc_t *h =
@@ -89,9 +125,9 @@ static void execute(skl_test_t *t) {
   const gemm_i8rcprc_i8rcprc_i32rcprc_t *h =
       (gemm_i8rcprc_i8rcprc_i32rcprc_t *)t->harness;
 
-  skl_gemm_a1b0_2x8_i8_i8c_i32_zvqwbdota8i(h->k1, h->a_pack.data, h->rsa1,
-                                            h->b_pack.data, h->csb1,
-                                            h->c_pack.data, h->rsc1);
+  skl_gemm_a1b0_i8_i8c_i32_zvqwbdota8i(h->m1, h->n1, h->k1, h->a_pack.data,
+                                       h->rsa1, h->b_pack.data, h->csb1,
+                                       h->c_pack.data, h->rsc1);
 }
 
 int main(void) {
